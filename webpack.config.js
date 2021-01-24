3bfbcd998587a3e00ffc43fb1828a574b0f0214e
Patrick Hulin <patrick.hulin@gmail.com>
2021-01-24 11:57:59 -0500

    Configuration files.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    garbo: './src/index.js'
+  },
+  mode: 'development',
+  devtool: false,
+  output: {
+    path: path.resolve(__dirname, 'KoLmafia', 'scripts', 'garbage-collector'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
+        // exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [],
+  externals: {
+    kolmafia: 'commonjs kolmafia',
+  },
+};
