4013ec464a777f475562dfd14e50bf7587204325
horrible little slime <69secret69email69@gmail.com>
2021-06-14 02:48:32 -0400

    checks for embezzler before throwing taffy, in case digitize counter is off; changes nostalgia monster to copyable

--- src/index.ts
+++ src/index.ts
@@ -264,7 +264,7 @@ function barfTurn() {
   } else {
     adventureMacroAuto(
       location,
-      Macro.externalIf(underwater, Macro.item("pulled green taffy")).meatKill()
+      Macro.externalIf(underwater, Macro.if_(`monsterid ${$monster`knob goblin embezzler`.id}`, Macro.item("pulled green taffy"))).meatKill()
     );
   }
 
