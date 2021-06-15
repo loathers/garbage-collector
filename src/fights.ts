48ae91cce3db5c670bf0e3dad415e13af72a19bb
horrible little slime <69secret69email69@gmail.com>
2021-06-15 01:30:30 -0400

    try to meteor shower when lecturing

--- src/fights.ts
+++ src/fights.ts
@@ -66,21 +66,21 @@ import { freeFightOutfit, meatOutfit, Requirement } from "./outfit";
 import { withStash } from "./stash";
 
 function checkFax(): boolean {
-  cliExecute('fax receive');
-  if (get('photocopyMonster') === $monster`Knob Goblin Embezzler`) return true;
-  cliExecute('fax send');
+  cliExecute("fax receive");
+  if (get("photocopyMonster") === $monster`Knob Goblin Embezzler`) return true;
+  cliExecute("fax send");
   return false;
 }
 
 function faxEmbezzler(): void {
-  if (!get('_photocopyUsed')){  
-    if (checkFax()) return; 
-    chatPrivate('cheesefax', 'Knob Goblin Embezzler');
+  if (!get("_photocopyUsed")) {
+    if (checkFax()) return;
+    chatPrivate("cheesefax", "Knob Goblin Embezzler");
     for (let i = 0; i < 3; i++) {
       wait(10);
-      if (checkFax()) return; 
+      if (checkFax()) return;
     }
-    abort("Failed to acquire photocopied Knob Goblin Embezzler.")
+    abort("Failed to acquire photocopied Knob Goblin Embezzler.");
   }
 }
 
@@ -93,7 +93,7 @@ export function dailyFights() {
       (!have($item`photocopied monster`) || get("photocopyMonster") !== embezzler) &&
       !get("_photocopyUsed")
     ) {
-      faxEmbezzler()
+      faxEmbezzler();
     }
 
     if (getClanLounge()["Clan pool table"] !== undefined) {
@@ -119,7 +119,8 @@ export function dailyFights() {
                 use($item`Platinum Yendorian Express Card`);
               }
             });
-            if (have($item`license to chill`) && !get("_licenseToChillUsed")) use($item`license to chill`);
+            if (have($item`license to chill`) && !get("_licenseToChillUsed"))
+              use($item`license to chill`);
 
             if (SourceTerminal.have()) SourceTerminal.educate([$skill`Extract`, $skill`Digitize`]);
 
@@ -131,9 +132,12 @@ export function dailyFights() {
             withMacro(
               Macro.if_(
                 "!hasskill Lecture on Relativity",
-                Macro.trySkill("Digitize").externalIf(
-                  !get("_cameraUsed") && !have($item`shaking 4-d camera`),
-                  Macro.tryItem("4-d camera")
+                Macro.trySkill("Meteor Shower").if_(
+                  "!hasskill Lecture on Relativity",
+                  Macro.trySkill("Digitize").externalIf(
+                    !get("_cameraUsed") && !have($item`shaking 4-d camera`),
+                    Macro.tryItem("4-d camera")
+                  )
                 )
               )
                 .trySkill("Lecture on Relativity")
@@ -633,13 +637,13 @@ const freeKillSources = [
   new FreeFight(
     () => (have($item`replica bat-oomerang`) ? clamp(3 - get("_usedReplicaBatoomerang"), 0, 3) : 0),
     () =>
-      withMacro(Macro.skill("Sing Along").item("replica bat-oomerang"), () => use($item`drum machine`)),
+      withMacro(Macro.skill("Sing Along").item("replica bat-oomerang"), () =>
+        use($item`drum machine`)
+      ),
     {
       familiar: () =>
         have($familiar`Trick-or-Treating Tot`) ? $familiar`Trick-or-Treating Tot` : null,
-      requirements: () => [
-        new Requirement(["100 Item Drop"], {}),
-      ],
+      requirements: () => [new Requirement(["100 Item Drop"], {})],
     }
   ),
 ];
@@ -661,17 +665,20 @@ export function freeFights() {
     useFamiliar(freeFightFamiliar());
     freeFightMood().execute();
     freeFightOutfit([new Requirement([], { forceEquip: $items`The Jokester's gun` })]);
-    if (questStep("questL08Trapper") >= 2){
-      adventureMacroAuto($location`Lair of the Ninja Snowmen`, Macro.skill("Sing Along").skill("Fire the Jokester's Gun"));
+    if (questStep("questL08Trapper") >= 2) {
+      adventureMacroAuto(
+        $location`Lair of the Ninja Snowmen`,
+        Macro.skill("Sing Along").skill("Fire the Jokester's Gun")
+      );
     } else if (have($skill`Comprehensive Cartography`) && get("_monstersMapped") < 3) {
-    try {
-      Macro.skill("Sing Along").skill("Fire the Jokester's Gun").setAutoAttack();
-      mapMonster($location`The Haiku Dungeon`, $monster`amateur ninja`);
-    } finally {
-      setAutoAttack(0);
+      try {
+        Macro.skill("Sing Along").skill("Fire the Jokester's Gun").setAutoAttack();
+        mapMonster($location`The Haiku Dungeon`, $monster`amateur ninja`);
+      } finally {
+        setAutoAttack(0);
+      }
     }
   }
-}
 
   try {
     for (const freeKillSource of freeKillSources) {
