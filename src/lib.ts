2da8beaab7f854771ee652c0462e797e1408e127
horrible little slime <69secret69email69@gmail.com>
2021-06-16 19:05:28 -0400

    squished in guzzlr

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,18 @@
-import { cliExecute, haveSkill, mallPrice, runChoice, toUrl, useSkill, visitUrl } from "kolmafia";
-import { $item, $skill, get, have, property, set } from "libram";
+import { canAdv } from "canadv.ash";
+import {
+  buy,
+  cliExecute,
+  haveSkill,
+  mallPrice,
+  print,
+  runChoice,
+  toItem,
+  toUrl,
+  use,
+  useSkill,
+  visitUrl,
+} from "kolmafia";
+import { $effect, $item, $items, $location, $skill, get, have, property, set } from "libram";
 
 export function setChoice(adventure: number, value: number) {
   set(`choiceAdventure${adventure}`, `${value}`);
@@ -102,4 +115,170 @@ export function voterSetup() {
   visitUrl(
     `choice.php?option=1&whichchoice=1331&g=${monsterVote}&local[]=${firstInit}&local[]=${secondInit}`
   );
+}
+interface zonePotion {
+  zone: String;
+  effect: Effect;
+  potion: Item;
+}
+
+const zonePotions = [
+  {
+    zone: "Spaaace",
+    effect: $effect`Transpondent`,
+    potion: $item`transporter transponder`,
+  },
+  {
+    zone: "Wormwood",
+    effect: $effect`absinthe-minded`,
+    potion: $item`tiny bottle of absinthe`,
+  },
+];
+
+export function prepWandererZone() {
+  const defaultLocation =
+    get("_spookyAirportToday") || get("spookyAirportAlways")
+      ? $location`the deep dark jungle`
+      : $location`noob cave`;
+  if (!have($item`guzzlr tablet`)) return defaultLocation;
+  if (get("questGuzzlr") === "unstarted") {
+    if (
+      get("_guzzlrPlatinumDeliveries") === 0 &&
+      get("guzzlrGoldDeliveries") >= 5 &&
+      (get("guzzlrPlatinumDeliveries") < 30 ||
+        (get("guzzlrGoldDeliveries") >= 150 && get("guzzlrBronzeDeliveries") >= 196))
+    ) {
+      set("choiceAdventure1412", 4);
+      use(1, $item`guzzlr tablet`);
+    } else if (
+      get("_guzzlrGoldDeliveries") < 3 &&
+      get("guzzlrBronzeDeliveries") >= 5 &&
+      (get("guzzlrGoldDeliveries") < 150 || get("guzzlrBronzeDeliveries") >= 196)
+    ) {
+      set("choiceAdventure1412", 3);
+      use(1, $item`guzzlr tablet`);
+    } else {
+      set("choiceAdventure1412", 2);
+      use(1, $item`guzzlr tablet`);
+    }
+  }
+
+  if (get("questGuzzlr") !== "unstarted") {
+    if (!guzzlrCheck() && !get("_guzzlrQuestAbandoned")) {
+      dropGuzzlrQuest();
+    }
+  }
+
+  if (get("questGuzzlr") === "unstarted") {
+    if (
+      get("_guzzlrPlatinumDeliveries") === 0 &&
+      get("guzzlrGoldDeliveries") >= 5 &&
+      (get("guzzlrPlatinumDeliveries") < 30 ||
+        (get("guzzlrGoldDeliveries") >= 150 && get("guzzlrBronzeDeliveries") >= 196))
+    ) {
+      set("choiceAdventure1412", 4);
+      use(1, $item`guzzlr tablet`);
+    } else if (
+      get("_guzzlrGoldDeliveries") < 3 &&
+      get("guzzlrBronzeDeliveries") >= 5 &&
+      (get("guzzlrGoldDeliveries") < 150 || get("guzzlrBronzeDeliveries") >= 196)
+    ) {
+      set("choiceAdventure1412", 3);
+      use(1, $item`guzzlr tablet`);
+    } else {
+      set("choiceAdventure1412", 2);
+      use(1, $item`guzzlr tablet`);
+    }
+  }
+
+  let freeFightZone = defaultLocation;
+  if (guzzlrCheck()) {
+    freeFightZone = get("guzzlrQuestLocation") || defaultLocation;
+    if (get("guzzlrQuestTier") === "platinum") {
+      zonePotions.forEach((place) => {
+        if (freeFightZone.zone === place.zone && !have(place.effect)) {
+          if (!have(place.potion)) {
+            buy(1, place.potion, 10000);
+          }
+          use(1, place.potion);
+        }
+      });
+    }
+  }
+  if (freeFightZone === get("guzzlrQuestLocation")) {
+    if (property.getString("guzzlrQuestBooze") === "Guzzlr cocktail set") {
+      if (
+        !$items`buttery boy, steamboat, ghiaccio colada, nog-on-the-cob, sourfinger`.some((drink) =>
+          have(drink)
+        )
+      ) {
+        cliExecute("make buttery boy");
+      }
+    } else {
+      const guzzlrBooze = toItem(get("guzzlrQuestBooze"));
+      if (guzzlrBooze === $item`none`) {
+        freeFightZone = defaultLocation;
+      } else if (!have(guzzlrBooze)) {
+        print(`just picking up some booze before we roll`, "blue");
+        cliExecute("acquire " + get("guzzlrQuestBooze"));
+      }
+    }
+  }
+  return freeFightZone;
+}
+
+function guzzlrCheck() {
+  const guzzlZone = get("guzzlrQuestLocation");
+  if (!guzzlZone) return false;
+  const forbiddenZones: String[] = ["The Rabbit Hole"]; //can't stockpile these potions,
+  if (!get("_spookyAirportToday") && !get("spookyAirportAlways")) {
+    forbiddenZones.push("Conspiracy Island");
+  }
+  if (!get("_stenchAirportToday") && !get("stenchAirportAlways")) {
+    forbiddenZones.push("Dinseylandfill");
+  }
+  if (!get("_hotAirportToday") && !get("hotAirportAlways")) {
+    forbiddenZones.push("That 70s Volcano");
+  }
+  if (!get("_coldAirportToday") && !get("coldAirportAlways")) {
+    forbiddenZones.push("The Glaciest");
+  }
+  if (!get("_sleazeAirportToday") && !get("sleazeAirportAlways")) {
+    forbiddenZones.push("Spring Break Beach");
+  }
+
+  zonePotions.forEach((place) => {
+    if (guzzlZone.zone === place.zone && have(place.effect)) {
+      if (!have(place.potion)) {
+        buy(1, place.potion, 10000);
+      }
+      use(1, place.potion);
+    }
+  });
+  if (
+    forbiddenZones.includes(guzzlZone.zone) ||
+    !guzzlZone.wanderers ||
+    guzzlZone === $location`The Oasis` ||
+    guzzlZone === $location`The Bubblin' Caldera` ||
+    guzzlZone.environment === "underwater" ||
+    guzzlZone === $location`Barrrney's Barrr` ||
+    guzzlZone === $location`The F'c'le` ||
+    guzzlZone === $location`the poop deck` ||
+    guzzlZone === $location`belowdecks` ||
+    guzzlZone === $location`the 8-bit realm` ||
+    (guzzlZone.zone === "BatHole" && guzzlZone !== $location`The Bat Hole Entrance`) ||
+    !canAdv(guzzlZone, false)
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function dropGuzzlrQuest() {
+  print("We hate this guzzlr quest!", "blue");
+  set("choiceAdventure1412", "");
+  visitUrl("inventory.php?tap=guzzlr", false);
+  runChoice(1);
+  runChoice(5);
 }
