f4fd9caa2b0bab6d9aed960328e46015e9e29737
horrible little slime <69secret69email69@gmail.com>
2021-06-17 12:56:39 -0400

    check the questpartyfair when deciding where to thesis

--- src/fights.ts
+++ src/fights.ts
@@ -705,7 +705,7 @@ function thesisReady(): boolean {
 }
 
 function deliverThesis(): void {
-  const thesisInNEP = get("neverendingPartyAlways") && get("_neverendingPartyFreeTurns") < 10;
+  const thesisInNEP = get("neverendingPartyAlways") && get("_neverendingPartyFreeTurns") < 10 && questStep("_questPartyFair") < 999;
 
   //Set up NEP if we haven't yet
   if (thesisInNEP) {
