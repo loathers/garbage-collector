746a6b59f15ff7c77d21b594b512bfb126ce81a3
horrible little slime <69secret69email69@gmail.com>
2021-06-17 21:28:22 -0400

    added beaten up check inside the while

--- src/index.ts
+++ src/index.ts
@@ -282,6 +282,7 @@ function barfTurn() {
     have($item`backup camera`) &&
     get<number>("_backUpUses") < 11
   ) {
+    if (have($effect`beaten up`)) throw "Hey, you're beaten up, and that's a bad thing. Lick your wounds, handle your problems, and run me again when you feel ready.";
     useFamiliar(meatFamiliar());
     meatOutfit(true, [new Requirement([], { forceEquip: $items`backup camera` })]);
     adventureMacro(
