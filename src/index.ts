5a1fd61ebc895f1e0c2110b5b85e1ca7192c26da
horrible little slime <69secret69email69@gmail.com>
2021-06-17 21:29:19 -0400

    made prettier

--- src/index.ts
+++ src/index.ts
@@ -266,7 +266,8 @@ function dailySetup() {
 }
 
 function barfTurn() {
-  if (have($effect`beaten up`)) throw "Hey, you're beaten up, and that's a bad thing. Lick your wounds, handle your problems, and run me again when you feel ready.";
+  if (have($effect`beaten up`))
+    throw "Hey, you're beaten up, and that's a bad thing. Lick your wounds, handle your problems, and run me again when you feel ready.";
   if (SourceTerminal.have()) {
     SourceTerminal.educate([$skill`Extract`, $skill`Digitize`]);
   }
@@ -282,7 +283,8 @@ function barfTurn() {
     have($item`backup camera`) &&
     get<number>("_backUpUses") < 11
   ) {
-    if (have($effect`beaten up`)) throw "Hey, you're beaten up, and that's a bad thing. Lick your wounds, handle your problems, and run me again when you feel ready.";
+    if (have($effect`beaten up`))
+      throw "Hey, you're beaten up, and that's a bad thing. Lick your wounds, handle your problems, and run me again when you feel ready.";
     useFamiliar(meatFamiliar());
     meatOutfit(true, [new Requirement([], { forceEquip: $items`backup camera` })]);
     adventureMacro(
