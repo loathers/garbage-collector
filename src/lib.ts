8f6ceb31ce4706d078d250d4c63bc612abea0874
horrible little slime <69secret69email69@gmail.com>
2021-06-20 00:15:20 -0400

    initial voting implementation

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,5 @@
 import { cliExecute, haveSkill, mallPrice, toUrl, useSkill, visitUrl } from "kolmafia";
-import { $skill, get, have, property, set } from "libram";
+import { $item, $skill, get, have, property, set } from "libram";
 
 export function setChoice(adventure: number, value: number) {
   set(`choiceAdventure${adventure}`, `${value}`);
@@ -54,4 +54,50 @@ export function questStep(questName: string) {
     }
     return parseInt(stringStep.substring(4), 10);
   }
+}
+
+export function voterSetup() {
+  if (have($item`"I Voted!" sticker`) || !(get("voteAlways") || get("_voteToday"))) return;
+  visitUrl("place.php?whichplace=town_right&action=townright_vote");
+
+  const votingMonsterPriority = [
+    "terrible mutant",
+    "angry ghost",
+    "government bureaucrat",
+    "annoyed snake",
+    "slime blob",
+  ];
+
+  const initPriority = new Map<string, number>([
+    ["Meat Drop: +30", 10],
+    ["Item Drop: +15", 9],
+    ["Familiar Experience: +2", 8],
+    ["Adventures: +1", 7],
+    ["Monster Level: +10", 5],
+    ["Meat Drop: -30", -1],
+    ["Item Drop: -15", -1],
+    ["Familiar Experience: -2", -1],
+  ]);
+
+  const monsterVote =
+    votingMonsterPriority.indexOf(get("_voteMonster1")) <
+    votingMonsterPriority.indexOf(get("_voteMonster2"))
+      ? 1
+      : 2;
+
+  const voteLocalPriorityArr = [
+    initPriority.get(get("_voteLocal1")) || 1,
+    initPriority.get(get("_voteLocal2")) || 1,
+    initPriority.get(get("_voteLocal3")) || 1,
+    initPriority.get(get("_voteLocal4")) || 1,
+  ];
+
+  const [firstPriority, secondPriority] = voteLocalPriorityArr.sort((a, b) => a - b).slice(0, 1);
+
+  const firstInit = voteLocalPriorityArr.indexOf(firstPriority);
+  const secondInit = voteLocalPriorityArr.indexOf(secondPriority);
+
+  visitUrl(
+    `choice.php?option=1&whichchoice=1331&g=${monsterVote}&local[]=${firstInit}&local[]=${secondInit}`
+  );
 }
