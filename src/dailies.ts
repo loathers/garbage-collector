7617d9efe5253288ee9ddc888815e17238d0fdb2
horrible little slime <69secret69email69@gmail.com>
2021-06-21 00:57:35 -0400

    make the volcano quest noisy again

--- src/dailies.ts
+++ src/dailies.ts
@@ -327,13 +327,12 @@ function checkVolcanoQuest() {
       if (tryToGetIt()) {
         if (volcanoItem !== $item`fused fuse`) {
           visitUrl("place.php?whichplace=airport_hot&action=airport4_questhub");
+          print(`Alright buddy, turning in ${volcanoItem.plural} for a volcoino!`);
           const choice =
             volcanoItems.indexOf(volcanoItem) === -1 ? 4 : 1 + volcanoItems.indexOf(volcanoItem);
           runChoice(choice);
         }
-        return volcanoItem;
       }
     }
   }
-  return $item`none`;
 }
