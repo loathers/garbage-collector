c697bf8004e01f386838982b0f2543bc6e1e0e02
horrible little slime <69secret69email69@gmail.com>
2021-06-22 16:34:44 -0400

    update types; libram isn't updated yet, and we have to be able to build this baby!

--- src/index.ts
+++ src/index.ts
@@ -281,9 +281,9 @@ function barfTurn() {
   }
 
   while (
-    get("lastCopyableMonster") === $monster`Knob Goblin Embezzler` &&
+    get<Monster>("lastCopyableMonster") === $monster`Knob Goblin Embezzler` &&
     have($item`backup camera`) &&
-    get("_backUpUses") < 11
+    get<number>("_backUpUses") < 11
   ) {
     if (have($effect`beaten up`))
       throw "Hey, you're beaten up, and that's a bad thing. Lick your wounds, handle your problems, and run me again when you feel ready.";
