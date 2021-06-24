f862ba6dd7e4016d4ab54c8bce87d9c5cc065db8
horrible-little-slime <78829653+horrible-little-slime@users.noreply.github.com>
2021-06-24 03:48:37 -0400

    updated Party Fair logic to initialize the quest properly (#127)
    * _questPartyFairQuest --> _questPartyFair
    
    * remove redundant logic

--- src/fights.ts
+++ src/fights.ts
@@ -833,9 +833,8 @@ const freeFightSources = [
         : 0,
     () => {
       setChoice(1324, 5); // pick fight.
-      if (get("_questPartyFairQuest") === "unstarted") {
+      if (get("_questPartyFair") === "unstarted") {
         visitUrl("adventure.php?snarfblat=528");
-        runChoice(["booze", "food"].includes(get("_questPartyFairQuest")) ? 1 : 2);
         if (get("_questPartyFairQuest") === "food") {
           runChoice(1);
           setChoice(1324, 2);
