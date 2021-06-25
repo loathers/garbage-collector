5ffd3edb1751cc023a8cbe6ea40da782576c4f94
horrible little slime <69secret69email69@gmail.com>
2021-06-24 20:32:56 -0400

    updated bumper logic

--- src/lib.ts
+++ src/lib.ts
@@ -9,6 +9,7 @@ import {
   maximize,
   myFamiliar,
   myPrimestat,
+  myTurncount,
   print,
   restoreMp,
   retrieveItem,
@@ -286,9 +287,13 @@ export class freeRun {
 
 const freeRuns: freeRun[] = [
   new freeRun(
-    () =>
-      getWorkshed() === $item`Asdon Martin keyfob` &&
-      !get("banishedMonsters").toLowerCase().includes("spring-loaded front bumper"),
+    () => {
+      if (getWorkshed() !== $item`Asdon Martin keyfob`) return false;
+      const banishes = get("banishedMonsters").split(":");
+      const bumperIndex = banishes.indexOf("spring-loaded ront bumper");
+      if (bumperIndex === -1) return true;
+      return myTurncount() - parseInt(banishes[bumperIndex + 1]) > 30;
+    },
     () => fillAsdonMartinTo(50),
     Macro.skill("Asdon Martin: Spring-Loaded Front Bumper")
   ),
