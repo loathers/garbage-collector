4504c07616ed3b45ac01ac72d920ce4820fead09
horrible-little-slime <78829653+horrible-little-slime@users.noreply.github.com>
2021-06-29 06:54:26 -0400

    daily expansion: pick tea! (#153)
    * select and then pick best tea
    
    * add pickTea() to dailySetup()
    
    * check for teatree ownership
    
    * check if you've harvested your tea tree yet
    
    * triple shakeval

--- src/dailies.ts
+++ src/dailies.ts
@@ -20,6 +20,7 @@ import {
   useSkill,
   availableAmount,
   equip,
+  getCampground,
 } from "kolmafia";
 import {
   have,
@@ -41,7 +42,7 @@ import {
   adventureMacro,
 } from "libram";
 import { meatFamiliar } from "./familiar";
-import { questStep, ensureEffect, setChoice, tryFeast, findRun } from "./lib";
+import { questStep, ensureEffect, setChoice, tryFeast, findRun, trueValue } from "./lib";
 import { withStash } from "./stash";
 
 export function voterSetup() {
@@ -400,4 +401,13 @@ export function gin() {
       cliExecute("farfuture drink");
     }
   }
+}
+
+const teas = $items`cuppa Activi tea, cuppa Alacri tea, cuppa Boo tea, cuppa Chari tea, cuppa Craft tea, cuppa Cruel tea, cuppa Dexteri tea, cuppa Feroci tea, cuppa Flamibili tea, cuppa Flexibili tea, cuppa Frost tea, cuppa Gill tea, cuppa Impregnabili tea, cuppa Improprie tea, cuppa Insani tea, cuppa Irritabili tea, cuppa Loyal tea, cuppa Mana tea, cuppa Mediocri tea, cuppa Monstrosi tea, cuppa Morbidi tea, cuppa Nas tea, cuppa Net tea, cuppa Neuroplastici tea, cuppa Obscuri tea, cuppa Physicali tea, cuppa Proprie tea, cuppa Royal tea, cuppa Serendipi Tea, cuppa Sobrie tea, cuppa Toast tea, cuppa Twen tea, cuppa Uncertain tea, cuppa Vitali tea, Cuppa Voraci tea, cuppa Wit tea, cuppa Yet tea`;
+export function pickTea() {
+  if (!getCampground()["potted tea tree"] || get("_pottedTeaTreeUsed")) return;
+  const bestTea = teas.sort((a, b) => trueValue(b) - trueValue(a))[0];
+  const shakeVal = 3 * trueValue(...teas);
+  const teaAction = shakeVal > trueValue(bestTea) ? "shake" : bestTea.name;
+  cliExecute(`teatree ${teaAction}`);
 }
