ea40f83dd02145b8c5b79ebd2c3f78d44195a500
Alistair Crook <acrook@acrook.co.uk>
2021-07-04 01:58:21 +0100

    Itemval fix

--- src/lib.ts
+++ src/lib.ts
@@ -857,7 +857,7 @@ function generateBjornList(mode: PickBjornMode): BjornedFamiliar[] {
     if (!familiar.modifier) return 0;
     const meatVal =
       mode === PickBjornMode.FREE ? 0 : baseMeat + mode === PickBjornMode.EMBEZZLER ? 750 : 0;
-    const itemVal = PickBjornMode.BARF ? 72 : 0;
+    const itemVal = mode === PickBjornMode.BARF ? 72 : 0;
     if (familiar.modifier.type === BjornModifierType.MEAT)
       return (familiar.modifier.modifier * meatVal) / 100;
     if (familiar.modifier.type === BjornModifierType.ITEM)
