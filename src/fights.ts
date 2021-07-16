a31eaccbd7b8a84434dfc68f65623281bb1ca48e
horrible little slime <69secret69email69@gmail.com>
2021-07-16 16:48:26 -0400

    implemegnt gname chagnge: name is a mandatory parameter at the top of the constructor function

--- src/fights.ts
+++ src/fights.ts
@@ -128,22 +128,22 @@ class EmbezzlerFight {
   run: (options: EmbezzlerFightOptions) => void;
   requirements: Requirement[];
   draggable: boolean;
-  name?: string;
+  name: string;
 
   constructor(
+    name: string,
     available: () => boolean,
     potential: () => number,
     run: (options: EmbezzlerFightOptions) => void,
     requirements: Requirement[] = [],
-    draggable = false,
-    name?: string
+    draggable = false
   ) {
+    this.name = name;
     this.available = available;
     this.potential = potential;
     this.run = run;
     this.requirements = requirements;
     this.draggable = draggable;
-    this.name = name;
   }
 }
 
@@ -194,6 +194,7 @@ const embezzlerMacro = () =>
 
 const embezzlerSources = [
   new EmbezzlerFight(
+    "Digitize",
     () =>
       get("_sourceTerminalDigitizeMonster") === $monster`Knob Goblin Embezzler` &&
       getCounters("Digitize Monster", 0, 0).trim() !== "",
@@ -205,6 +206,7 @@ const embezzlerSources = [
     true
   ),
   new EmbezzlerFight(
+    "Backup",
     () =>
       get("lastCopyableMonster") === $monster`Knob Goblin Embezzler` &&
       have($item`backup camera`) &&
@@ -229,10 +231,10 @@ const embezzlerSources = [
         bonusEquip: new Map([[$item`backup camera`, 5000]]),
       }),
     ],
-    true,
-    "Backup"
+    true
   ),
   new EmbezzlerFight(
+    "Fax",
     () => have($item`Clan VIP Lounge key`) && !get("_photocopyUsed"),
     () => (have($item`Clan VIP Lounge key`) && !get("_photocopyUsed") ? 1 : 0),
     () => {
@@ -241,6 +243,7 @@ const embezzlerSources = [
     }
   ),
   new EmbezzlerFight(
+    "Pillkeeper Semirare",
     () =>
       have($item`Eight Days a Week Pill Keeper`) &&
       canAdv($location`Knob Treasury`, true) &&
@@ -257,6 +260,7 @@ const embezzlerSources = [
     }
   ),
   new EmbezzlerFight(
+    "Chateau Painting",
     () =>
       ChateauMantegna.have() &&
       !ChateauMantegna.paintingFought() &&
@@ -270,6 +274,7 @@ const embezzlerSources = [
     () => ChateauMantegna.fightPainting()
   ),
   new EmbezzlerFight(
+    "Spooky Putty",
     () =>
       have($item`Spooky putty monster`) &&
       get("spookyPuttyMonster") === $monster`Knob Goblin Embezzler`,
@@ -288,6 +293,7 @@ const embezzlerSources = [
     () => use($item`Spooky putty monster`)
   ),
   new EmbezzlerFight(
+    "4-d Camera",
     () =>
       have($item`shaking 4-d camera`) &&
       get("cameraMonster") === $monster`Knob Goblin Embezzler` &&
@@ -301,6 +307,7 @@ const embezzlerSources = [
     () => use($item`shaking 4-d camera`)
   ),
   new EmbezzlerFight(
+    "Green Taffy",
     () =>
       have($item`envyfish egg`) &&
       get("envyfishMonster") === $monster`Knob Goblin Embezzler` &&
@@ -314,11 +321,13 @@ const embezzlerSources = [
     () => use($item`envyfish egg`)
   ),
   new EmbezzlerFight(
+    "Professor MeatChain",
     () => false,
     () => (have($familiar`Pocket Professor`) && !get<boolean>("_garbo_meatChain", false) ? 1 : 0),
     () => {}
   ),
   new EmbezzlerFight(
+    "Professor WeightChain",
     () => false,
     () => (have($familiar`Pocket Professor`) && !get<boolean>("_garbo_weightChain", false) ? 1 : 0),
     () => {}
@@ -361,6 +370,7 @@ function getEmbezzlerFight(): EmbezzlerFight | null {
     )
   ) {
     return new EmbezzlerFight(
+      "Pocket Wish",
       () => false,
       () => 0,
       () => {
