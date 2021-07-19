1b0af1daf4d82135e3b7b7d92531591b77b38c5b
horrible little slime <69secret69email69@gmail.com>
2021-07-19 16:00:28 -0400

    use post

--- src/dailies.ts
+++ src/dailies.ts
@@ -134,7 +134,7 @@ export function configureGear(): void {
 export function latte(): void {
   const latte = $item`latte lovers member's mug`;
   if (have(latte) && questStep("questL02Larva") > -1 && questStep("questL11MacGuffin") > -1) {
-    visitUrl("main.php?latte=1", false);
+    visitUrl("main.php?latte=1");
     runChoice(2);
     if (
       numericModifier(latte, "Familiar Weight") !== 5 ||
