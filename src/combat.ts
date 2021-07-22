ffd6d74a8265e205934dce6a30d428a1b21d4f2e
CheeseyPickle <86745595+CheeseyPickle@users.noreply.github.com>
2021-07-21 22:11:21 -0500

    Add feel nostalgic support (#285)
    * Add feel nostalgic support
    
    Probably a really minor optimization, but it's not like it gets used otherwise
    
    * fixed linting issue

--- src/combat.ts
+++ src/combat.ts
@@ -88,6 +88,10 @@ export class Macro extends LibramMacro {
         Macro.if_("monstername garbage tourist", Macro.trySkill("Gallapagosian Mating Call"))
       )
       .externalIf(
+        get("lastCopyableMonster") === $monster`garbage tourist`,
+        Macro.if_("!monstername garbage tourist", Macro.trySkill("Feel Nostalgic"))
+      )
+      .externalIf(
         myFamiliar() === $familiar`Stocking Mimic`,
         Macro.skill("Curse of Weaksauce").while_("!pastround 10", Macro.item("seal tooth"))
       )
