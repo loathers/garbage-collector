ceecaef5166392856669b27c953e4225aa9524bd
horrible little slime <69secret69email69@gmail.com>
2021-07-23 01:19:27 -0400

    implement estimatedTurns()

--- src/globalvars.ts
+++ src/globalvars.ts
@@ -1,3 +1,6 @@
+import { inebrietyLimit, myAdventures, myInebriety } from "kolmafia";
+import { $item, have } from "libram";
+
 export const log = {
   initialEmbezzlersFought: 0,
   digitizedEmbezzlersFought: 0,
@@ -7,3 +10,10 @@ export const globalOptions: { ascending: boolean; stopTurncount: number | null }
   stopTurncount: null,
   ascending: false,
 };
+
+export function estimatedTurns(): number {
+  return (
+    (myAdventures() + (globalOptions.ascending && myInebriety() < inebrietyLimit() ? 60 : 0)) *
+    (have($item`mafia thumb ring`) ? 1.04 : 1)
+  );
+}
