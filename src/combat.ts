f2f0465f563996a011d0de5924fc4f8f86f873da
Rinn <Kirchoff.Joseph.P@gmail.com>
2021-08-07 12:40:40 -0700

    Try and get sauceror weaksauce mp

--- src/combat.ts
+++ src/combat.ts
@@ -284,16 +284,25 @@ export class Macro extends LibramMacro {
   }
 
   kill(): Macro {
-    // Using while_ here in case you run out of mp
-    return this.while_("!pastround 20 && !hppercentbelow 25 && !missed 1", Macro.attack())
-      .while_("hasskill Saucegeyser", Macro.skill($skill`Saucegeyser`))
-      .while_("hasskill Weapon of the Pastalord", Macro.skill($skill`Weapon of the Pastalord`))
-      .while_("hasskill Saucestorm", Macro.skill($skill`Saucestorm`))
-      .while_("hasskill Cannelloni Cannon", Macro.skill($skill`Cannelloni Cannon`))
-      .while_("hasskill Wave of Sauce", Macro.skill($skill`Wave of Sauce`))
-      .while_("hasskill Lunging Thrust-Smack", Macro.skill($skill`Lunging Thrust-Smack`))
-      .attack()
-      .repeat();
+    return (
+      this.externalIf(
+        myClass() === $class`Sauceror`,
+        Macro.tryHaveSkill($skill`Curse of Weaksauce`)
+      )
+        .externalIf(
+          myClass() !== $class`Sauceror`,
+          Macro.while_("!pastround 20 && !hppercentbelow 25 && !missed 1", Macro.attack())
+        )
+        // Using while_ here in case you run out of mp
+        .while_("hasskill Saucegeyser", Macro.skill($skill`Saucegeyser`))
+        .while_("hasskill Weapon of the Pastalord", Macro.skill($skill`Weapon of the Pastalord`))
+        .while_("hasskill Cannelloni Cannon", Macro.skill($skill`Cannelloni Cannon`))
+        .while_("hasskill Wave of Sauce", Macro.skill($skill`Wave of Sauce`))
+        .while_("hasskill Saucestorm", Macro.skill($skill`Saucestorm`))
+        .while_("hasskill Lunging Thrust-Smack", Macro.skill($skill`Lunging Thrust-Smack`))
+        .attack()
+        .repeat()
+    );
   }
 
   static kill(): Macro {
