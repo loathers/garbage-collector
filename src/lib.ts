e05ca1d3692350d5fc2986492324d9c6b8067ceb
CheeseyPickle <86745595+CheeseyPickle@users.noreply.github.com>
2021-08-07 23:56:42 -0500

    Use parasols if it won't break and you're ascending (#340)

--- src/lib.ts
+++ src/lib.ts
@@ -35,6 +35,7 @@ import {
   property,
   SongBoom,
 } from "libram";
+import { globalOptions } from "./globalvars";
 
 export const propertyManager = new PropertiesManager();
 
@@ -431,6 +432,16 @@ const freeRuns: FreeRun[] = [
     () => have($item`Greatest American Pants`) && get("_navelRunaways") < 3,
     Macro.trySkill("Asdon Martin: Spring-Loaded Front Bumper").step("runaway"),
     new Requirement([], { forceEquip: $items`Greatest American Pants` })
+  ),
+
+  new FreeRun(
+    "Parasol",
+    () =>
+      have($item`peppermint parasol`) &&
+      globalOptions.ascending &&
+      get("parasolUsed") < 9 &&
+      get("_navelRunaways") < 3,
+    Macro.trySkill("Asdon Martin: Spring-Loaded Front Bumper").item("peppermint parasol")
   ),
 ];
 
