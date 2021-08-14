cb531957710010bee8e3a574d885058bbc5a9cc8
Rinn <Kirchoff.Joseph.P@gmail.com>
2021-08-14 10:42:17 -0700

    fix

--- src/fights.ts
+++ src/fights.ts
@@ -1343,7 +1343,7 @@ const freeRunFightSources = [
     },
     {
       familiar: () => $familiar`Space Jellyfish`,
-      requirements: () => [new Requirement([], { forceEquip = $items`Powerful Glove` })],
+      requirements: () => [new Requirement([], { forceEquip: $items`Powerful Glove` })],
     }
   ),
   new FreeFight(
