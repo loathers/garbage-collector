c47e31fc1c9bc35be563f76aecbc0f4d49aaa906
horrible little slime <69secret69email69@gmail.com>
2021-08-18 13:02:30 -0400

    make computers less bad

--- src/dailies.ts
+++ src/dailies.ts
@@ -344,9 +344,8 @@ function checkVolcanoQuest() {
     )
     .sort((a, b) => b.price - a.price);
 
-  if (volcanoItems !== []) {
+  if (volcanoItems.length) {
     const chosenItem = volcanoItems[0];
-    if (!chosenItem) return;
     if (chosenItem.item === $item`fused fuse`) {
       globalOptions.messages.push("Remember to nab a fused fuse with your stooper!");
     } else {
