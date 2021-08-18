fc9ec8f811c6b0570d7432c097a2ad5ba5902f96
horrible little slime <69secret69email69@gmail.com>
2021-08-18 02:59:48 -0400

    rearrange predicates to read a little better

--- src/outfit.ts
+++ src/outfit.ts
@@ -109,13 +109,13 @@ export function tryFillLatte(): boolean {
   if (
     have($item`latte lovers member's mug`) &&
     get("_latteRefillsUsed") < 3 &&
-    ((get("latteUnlocks").includes("cajun") &&
-      get("latteUnlocks").includes("rawhide") &&
-      (numericModifier($item`latte lovers member's mug`, "Familiar Weight") !== 5 ||
-        numericModifier($item`latte lovers member's mug`, "Meat Drop") !== 40 ||
-        (get("latteUnlocks").includes("carrot") &&
-          numericModifier($item`latte lovers member's mug`, "Item Drop") !== 20))) ||
-      get("_latteCopyUsed"))
+    (get("_latteCopyUsed") ||
+      (get("latteUnlocks").includes("cajun") &&
+        get("latteUnlocks").includes("rawhide") &&
+        (numericModifier($item`latte lovers member's mug`, "Familiar Weight") !== 5 ||
+          numericModifier($item`latte lovers member's mug`, "Meat Drop") !== 40 ||
+          (get("latteUnlocks").includes("carrot") &&
+            numericModifier($item`latte lovers member's mug`, "Item Drop") !== 20))))
   ) {
     const latteIngredients = [
       "cajun",
