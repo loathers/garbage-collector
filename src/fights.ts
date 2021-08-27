71e13929bf680db5d70b97050c391b6d2636e389
horrible little slime <69secret69email69@gmail.com>
2021-08-27 18:44:51 -0400

    fix enamorang counter name

--- src/fights.ts
+++ src/fights.ts
@@ -545,7 +545,7 @@ function startDigitize() {
   if (
     (getCounters("Digitize Monster", 0, 100).trim() === "" &&
       get("_sourceTerminalDigitizeUses") !== 0) ||
-    (!getCounters("Lov Enamorang", 0, 100).trim() && get("enamorangMonster"))
+    (!getCounters("Enamorang monster", 0, 100).trim() && get("enamorangMonster"))
   ) {
     do {
       const run = findRun() || ltbRun;
