2ed010e551a37e523abcb3fb88104d5004c9d568
horrible little slime <69secret69email69@gmail.com>
2021-09-01 01:56:40 -0400

    emergency patch for escaped commas

--- src/mood.ts
+++ src/mood.ts
@@ -180,11 +180,13 @@ export function freeFightMood(): Mood {
 }
 
 function shrugPassiveDamage() {
-  $effects`Apoplectic with Rage, Barfpits, Berry Thorny, Biologically Shocked, Bone Homie, Boner Battalion, Burning\, Man, Coal-Powered, Curse of the Black Pearl Onion, Dizzy with Rage, Drenched With Filth, EVISCERATE!, Fangs and Pangs, Frigidalmatian, Gummi Badass, Haiku State of Mind, It's Electric!, Jabañero Saucesphere, Jalapeño Saucesphere, Little Mouse Skull Buddy, Long Live GORF, Mayeaugh, Permanent Halloween, Psalm of Pointiness, Pygmy Drinking Buddy, Quivering with Rage, Scarysauce, Skeletal Cleric, Skeletal Rogue, Skeletal Warrior, Skeletal Wizard, Smokin', Soul Funk, Spiky Frozen Hair, Stinkybeard, Stuck-Up Hair, Can Has Cyborger, Yes\, Can Haz, Feeling Nervous`.forEach(
-    (effect) => {
-      if (have(effect)) {
-        uneffect(effect);
-      }
+  [
+    ...$effects`Apoplectic with Rage, Barfpits, Berry Thorny, Biologically Shocked, Bone Homie, Boner Battalion, Coal-Powered, Curse of the Black Pearl Onion, Dizzy with Rage, Drenched With Filth, EVISCERATE!, Fangs and Pangs, Frigidalmatian, Gummi Badass, Haiku State of Mind, It's Electric!, Jabañero Saucesphere, Jalapeño Saucesphere, Little Mouse Skull Buddy, Long Live GORF, Mayeaugh, Permanent Halloween, Psalm of Pointiness, Pygmy Drinking Buddy, Quivering with Rage, Scarysauce, Skeletal Cleric, Skeletal Rogue, Skeletal Warrior, Skeletal Wizard, Smokin', Soul Funk, Spiky Frozen Hair, Stinkybeard, Stuck-Up Hair, Can Has Cyborger, Feeling Nervous`,
+    $effect`Burning, Man`,
+    $effect`Yes, Can Haz`,
+  ].forEach((effect) => {
+    if (have(effect)) {
+      uneffect(effect);
     }
-  );
+  });
 }
