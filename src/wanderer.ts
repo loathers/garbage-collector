6e8e50f4a9866e193d529e25dba99d45419eb064
horrible little slime <69secret69email69@gmail.com>
2021-10-10 15:36:45 -0400

    finally make everything work right

--- src/wanderer.ts
+++ src/wanderer.ts
@@ -21,7 +21,7 @@ export enum draggableFight {
 function untangleDigitizes(turnCount: number, chunks: number): number {
   const turnsPerChunk = turnCount / chunks;
   const monstersPerChunk = Math.sqrt((turnsPerChunk + 3) / 5 + 1 / 4) - 1 / 2;
-  return chunks * monstersPerChunk;
+  return Math.round(chunks * monstersPerChunk);
 }
 
 export function digitizedMonstersRemaining(): number {
@@ -39,8 +39,7 @@ export function digitizedMonstersRemaining(): number {
   if (turnsLeftAtNextMonster <= 0) return 0;
   const turnsAtLastDigitize = turnsLeftAtNextMonster + ((monsterCount + 1) * monsterCount * 5 - 3);
   return (
-    Math.round(untangleDigitizes(turnsAtLastDigitize, digitizesLeft + 1) * (1 + digitizesLeft)) /
-      3 -
+    untangleDigitizes(turnsAtLastDigitize, digitizesLeft + 1) -
     get("_sourceTerminalDigitizeMonsterCount")
   );
 }
