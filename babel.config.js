c4fe565a63efd5d3b1683e419700a5ef6409d8d1
horrible little slime <69secret69email69@gmail.com>
2021-10-17 23:22:41 -0400

    upgrade babel & remove patch files

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,7 @@ module.exports = function (api) {
       [
         "@babel/preset-env",
         {
-          targets: { rhino: "1.7" },
+          targets: { rhino: "1.7.13" },
         },
       ],
     ],
