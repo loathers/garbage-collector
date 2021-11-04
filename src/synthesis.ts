db63fdb8e63d7313e659d25f024f3cc19b049ad8
horrible little slime <69secret69email69@gmail.com>
2021-11-04 14:23:27 -0400

    rejigger synthesis code to take casts rather than turns as th einput

--- src/synthesis.ts
+++ src/synthesis.ts
@@ -1,6 +1,5 @@
 import {
   availableAmount,
-  haveEffect,
   itemAmount,
   mySpleenUse,
   spleenLimit,
@@ -11,13 +10,11 @@ import { $items, clamp } from "libram";
 import { shuffle } from "./lib";
 
 const whitelist = $items`sugar shotgun, sugar shillelagh, sugar shank, sugar chapeau, sugar shorts, sugar shield, sugar shirt, Fudgie Roll`;
-export default function synthesize(effect: Effect, minTurns: number): void {
+export default function synthesize(effect: Effect, casts: number): void {
   const shuffledWhitelist = shuffle([...whitelist]);
   for (const itemA of shuffledWhitelist) {
-    if (haveEffect(effect) >= minTurns) return;
     if (availableAmount(itemA) <= 1) continue;
     for (const itemB of shuffledWhitelist) {
-      if (haveEffect(effect) >= minTurns) return;
       const minimum = itemA === itemB ? 2 : 1;
       if (availableAmount(itemB) <= minimum) continue;
       if (sweetSynthesisResult(itemA, itemB) !== effect) continue;
@@ -25,16 +22,15 @@ export default function synthesize(effect: Effect, minTurns: number): void {
         itemA === itemB
           ? Math.floor((itemAmount(itemA) - 1) / 2)
           : Math.min(itemAmount(itemA), itemAmount(itemB)) - 1;
-      const neededCasts = Math.floor((minTurns - haveEffect(effect)) / 30);
       const spleen = Math.max(spleenLimit() - mySpleenUse(), 0);
-      const casts = Math.min(possibleCasts, neededCasts, spleen);
-      if (casts === 0) continue;
-      sweetSynthesis(casts, itemA, itemB);
+      const castsToDo = Math.min(possibleCasts, casts, spleen);
+      if (castsToDo === 0) continue;
+      sweetSynthesis(castsToDo, itemA, itemB);
+      casts -= castsToDo;
     }
   }
 
-  const neededCasts = Math.floor((minTurns - haveEffect(effect)) / 30);
-  for (let i = 0; i < clamp(neededCasts, 0, spleenLimit() - mySpleenUse()); i++) {
+  for (let i = 0; i < clamp(casts, 0, spleenLimit() - mySpleenUse()); i++) {
     sweetSynthesis(effect);
   }
 }
