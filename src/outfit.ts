8eff97910e18479d9c9371f50585678d73b90b09
horrible-little-slime <78829653+horrible-little-slime@users.noreply.github.com>
2021-11-16 21:15:55 -0500

    prevent all our various equips within the outfit functions (#552)

--- src/outfit.ts
+++ src/outfit.ts
@@ -87,7 +87,10 @@ export function freeFightOutfit(requirements: Requirement[] = []): void {
 
   const finalRequirement = new Requirement(parameters, {
     forceEquip: forceEquip,
-    preventEquip: preventEquip,
+    preventEquip: [
+      ...preventEquip,
+      ...$items`broken champagne bottle, Spooky Putty snake, Spooky Putty mitre, Spooky Putty leotard, Spooky Putty ball, papier-mitre, smoke ball`,
+    ],
     bonusEquip: new Map<Item, number>([
       ...bonusEquip,
       ...dropsItems(equipMode),
@@ -228,7 +231,7 @@ export function meatOutfit(
       {
         forceEquip,
         preventEquip: [
-          ...$items`broken champagne bottle`,
+          ...$items`broken champagne bottle, Spooky Putty snake, Spooky Putty mitre, Spooky Putty leotard, Spooky Putty ball, papier-mitre, smoke ball`,
           ...(embezzlerUp ? $items`cheap sunglasses` : []),
           bjornAlike === $item`Buddy Bjorn` ? $item`Crown of Thrones` : $item`Buddy Bjorn`,
         ],
