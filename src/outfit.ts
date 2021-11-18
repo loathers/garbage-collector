1830ba0dfd0c0500a53cb2180e1e6e79b86ff19b
horrible-little-slime <78829653+horrible-little-slime@users.noreply.github.com>
2021-11-18 16:10:56 -0500

    add preliminary support for the daylight shavings helmet (#553)
    * add preliminary support for the daylight shavings helmet
    
    * move verify-constants line

--- src/outfit.ts
+++ src/outfit.ts
@@ -25,6 +25,7 @@ import {
 import {
   $class,
   $effect,
+  $effects,
   $familiar,
   $item,
   $items,
@@ -96,6 +97,7 @@ export function freeFightOutfit(requirements: Requirement[] = []): void {
       ...dropsItems(equipMode),
       ...pantsgiving(),
       ...cheeses(false),
+      ...shavingBonus(),
       ...(bjornAlike
         ? new Map<Item, number>([
             [
@@ -239,6 +241,7 @@ export function meatOutfit(
           ...dropsItems(equipMode),
           ...(embezzlerUp ? [] : pantsgiving()),
           ...cheeses(embezzlerUp),
+          ...shavingBonus(),
           ...(bjornAlike
             ? new Map<Item, number>([
                 [
@@ -415,4 +418,21 @@ function bestBjornalike(existingForceEquips: Item[]): Item | undefined {
     return $item`Crown of Thrones`;
   }
   return $item`Buddy Bjorn`;
+}
+
+function shavingBonus(): Map<Item, number> {
+  // eslint-disable-next-line libram/verify-constants
+  if (!have($item`Daylight Shavings Helmet`)) return new Map();
+  if (
+    // eslint-disable-next-line libram/verify-constants
+    $effects`Barbell Moustache, Cowboy Stache, Friendly Chops, Grizzly Beard, Gull-Wing Moustache, Musician's Musician's Moustache, Pointy Wizard Beard, Space Warlord's Beard, Spectacle Moustache, Surrealist's Moustache, Toiletbrush Moustache`.some(
+      (effect) => have(effect)
+    )
+  ) {
+    return new Map();
+  }
+
+  const bonusValue = (baseMeat * 100 + 72 * 50) / 100;
+  // eslint-disable-next-line libram/verify-constants
+  return new Map<Item, number>([[$item`Daylight Shavings Helmet`, bonusValue]]);
 }
