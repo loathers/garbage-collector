4134ef1029bc511fa5915790083af40949994a5a
Skaazi <49845237+s-k-z@users.noreply.github.com>
2021-11-28 10:26:13 -0700

    Log items and quantities being acquired (#573)
    * Log items and quantities being acquired
    
    * no plural
    
    Co-authored-by: Skaazi <skaazi@protonmail.com>

--- src/acquire.ts
+++ src/acquire.ts
@@ -38,15 +38,19 @@ export function acquire(qty: number, item: Item, maxPrice?: number, throwOnFail 
   let remaining = qty - startAmount;
   if (remaining <= 0) return qty;
 
+  const logError = (target: Item, source: string) => {
+    throw `Failed to remove ${target} from ${source}`;
+  };
+
   if (get("autoSatisfyWithCloset")) {
     const getCloset = Math.min(remaining, closetAmount(item));
-    if (!takeCloset(getCloset, item) && throwOnFail) throw "failed to remove from closet";
+    if (!takeCloset(getCloset, item) && throwOnFail) logError(item, "closet");
     remaining -= getCloset;
     if (remaining <= 0) return qty;
   }
 
   const getStorage = Math.min(remaining, storageAmount(item));
-  if (!takeStorage(getStorage, item) && throwOnFail) throw "failed to remove from storage";
+  if (!takeStorage(getStorage, item) && throwOnFail) logError(item, "storage");
   remaining -= getStorage;
   if (remaining <= 0) return qty;
 
@@ -56,7 +60,7 @@ export function acquire(qty: number, item: Item, maxPrice?: number, throwOnFail 
     cliExecute("refresh inventory");
     remaining = qty - itemAmount(item);
     getMall = Math.min(remaining, shopAmount(item));
-    if (!takeShop(getMall, item) && throwOnFail) throw "failed to remove from shop";
+    if (!takeShop(getMall, item) && throwOnFail) logError(item, "shop");
   }
   remaining -= getMall;
   if (remaining <= 0) return qty;
