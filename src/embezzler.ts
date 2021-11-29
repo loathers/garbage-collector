8def03df72d900654b8fddebbd2d322783ac4da6
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2021-11-29 08:29:30 -0600

    Update src/embezzler.ts
    Co-authored-by: horrible-little-slime <78829653+horrible-little-slime@users.noreply.github.com>

--- src/embezzler.ts
+++ src/embezzler.ts
@@ -50,7 +50,7 @@ export class EmbezzlerFight {
    * This is the class that creates all the different ways to fight embezzlers
    * @classdesc Something goes here
    * @prop {string} name The name of the source of this fight, primarily used to identify special cases.
-   * @prop {boolean} available this should be a statement that checks the availability of the fight
+   * @prop {() => boolean} available Returns whether or not we can do this fight right now.
    * @prop {number} potential this should be a statement that checks the number of this type of embezzler fight available
    * @prop {Location, Macro} run This runs the combat, using the macro and location provided, if any
    * @prop {[]} requirements This is an array of requirements to do this fight
