62788cb01b01dd5761282eae926edac5e663c975
horrible little slime <69secret69email69@gmail.com>
2021-12-02 13:35:46 -0500

    remove final horseradish

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,6 @@ function ensureBarfAccess() {
 
 function barfTurn() {
   const startTurns = totalTurnsPlayed();
-  horseradish();
   if (have($effect`Beaten Up`))
     throw "Hey, you're beaten up, and that's a bad thing. Lick your wounds, handle your problems, and run me again when you feel ready.";
   if (SourceTerminal.have()) {
