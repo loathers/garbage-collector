8bf1e6631bec15a756a488cc528944ecfbc7eca1
horrible little slime <69secret69email69@gmail.com>
2021-12-02 15:53:03 -0500

    orb, glove

--- src/extravermectin.ts
+++ src/extravermectin.ts
@@ -4,4 +4,7 @@ export function expectedGregs(): number {
   let gregs = 3;
   if (have($item`Time-Spinner`)) gregs += Math.floor((10 - get("_timeSpinnerMinutesUsed")) / 3);
   if (have($skill`Meteor Lore`)) gregs += 10 - get("_macrometeoriteUses");
+  if (have($item`Powerful Glove`))
+    gregs += Math.floor((100 - get("_powerfulGloveBatteryPowerUsed")) / 10);
+  if (have($item`miniature crystal ball`)) gregs += 3;
 }
