f93407d88138cc292545be21c36fa0d35937a30d
horrible little slime <69secret69email69@gmail.com>
2021-12-03 17:33:51 -0500

    calculate monsterReplacerGregs pre rever's wisdom

--- src/extrovermectin.ts
+++ src/extrovermectin.ts
@@ -1,4 +1,4 @@
-import { $item, $skill, get, have } from "libram";
+import { $item, $skill, clamp, get, have } from "libram";
 
 export function expectedGregs(): number {
   const baseGregs = 3;
@@ -8,15 +8,21 @@ export function expectedGregs(): number {
   const orbGregs = have($item`miniature crystal ball`) ? 1 : 0;
 
   const macrometeors = have($skill`Meteor Lore`) ? 10 - get("_macrometeoriteUses") : 0;
-  const replaceEnemies = have($item`powerful glove`)
+  const replaceEnemies = have($item`Powerful Glove`)
     ? Math.floor((100 - get("_powerfulGloveBatteryPowerUsed")) / 10)
     : 0;
   const totalMonsterReplacers = macrometeors + replaceEnemies;
 
-  const sabersLeft = have($item`fourth of may cosplay saber`)
+  const sabersLeft = have($item`Fourth of May Cosplay Saber`)
     ? clamp(5 - get("_saberForceUses"), 0, 3)
     : 0;
 
+  const baseRateMultiplier = have($skill`Transcendent Olfaction`) ? 0.95 : 0.75;
+  const monsterReplacerGregs = clamp(
+    totalMonsterReplacers,
+    0,
+    2 * sabersLeft + baseRateMultiplier * (totalMonsterReplacers - 2 * sabersLeft)
+  );
   const gregs = baseGregs + timeSpunGregs + orbGregs + monsterReplacerGregs;
   return gregs;
 }
