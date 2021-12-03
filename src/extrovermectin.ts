8b156b57fc886a14c5f12b97ce1961999b72961b
horrible little slime <69secret69email69@gmail.com>
2021-12-03 00:40:00 -0500

    restructure code a little

--- src/extrovermectin.ts
+++ src/extrovermectin.ts
@@ -1,10 +1,16 @@
 import { $item, $skill, get, have } from "libram";
 
 export function expectedGregs(): number {
-  let gregs = 3;
-  if (have($item`Time-Spinner`)) gregs += Math.floor((10 - get("_timeSpinnerMinutesUsed")) / 3);
-  if (have($skill`Meteor Lore`)) gregs += 10 - get("_macrometeoriteUses");
-  if (have($item`Powerful Glove`))
-    gregs += Math.floor((100 - get("_powerfulGloveBatteryPowerUsed")) / 10);
-  if (have($item`miniature crystal ball`)) gregs += 3;
+  const baseGregs = 3;
+  const timeSpunGregs =  (have($item`Time-Spinner`)) ? Math.floor((10 - get("_timeSpinnerMinutesUsed")) / 3) : 0;
+  const orbGregs = have($item`miniature crystal ball`) ? 1 : 0
+  
+  const macrometeors = have($skill`Meteor Lore`) ? 10 - get("_macrometeoriteUses") : 0;
+  const replaceEnemies = have($item`powerful glove`) ? Math.floor((100 - get("_powerfulGloveBatteryPowerUsed")) / 10) : 0;
+  const totalMonsterReplacers = macrometeors + replaceEnemies;
+
+  const monsterReplacerGregs = //stuff;
+  
+
+  return gregs;
 }
