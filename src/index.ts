3e02186bddefaed9a8e17d8dd3c61ec154df8726
horrible little slime <69secret69email69@gmail.com>
2021-12-25 22:43:45 -0500

    fully revert changes to barfTurn

--- src/index.ts
+++ src/index.ts
@@ -194,11 +194,12 @@ function barfTurn() {
         location,
         Macro.externalIf(
           underwater,
-          Macro.if_(
-            $monster`Knob Goblin Embezzler`,
-            Macro.item($item`pulled green taffy`)
-          ).meatKill()
-        ).abort()
+          Macro.if_($monster`Knob Goblin Embezzler`, Macro.item($item`pulled green taffy`))
+        ).meatKill(),
+        Macro.if_(
+          `(monsterid ${$monster`Knob Goblin Embezzler`.id}) && !gotjump && !(pastround 2)`,
+          Macro.externalIf(underwater, Macro.item($item`pulled green taffy`)).meatKill()
+        )
       );
     }
   }
