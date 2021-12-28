ad184e1b6fac73cba00ab95016e2f8f2febe22f1
horrible little slime <69secret69email69@gmail.com>
2021-12-28 16:50:54 -0500

    account for when the romantic monster window end actually is -1

--- src/embezzler.ts
+++ src/embezzler.ts
@@ -162,7 +162,9 @@ export const embezzlerSources = [
     () =>
       get("_romanticFightsLeft") > 0 &&
       getCounter("Romantic Monster window begin") <= 0 &&
-      getCounter("Romantic Monster window end") <= 0,
+      getCounter("Romantic Monster window end") <= 0 &&
+      (getCounter("Romantic Monster window end") !== -1 ||
+        getCounters("Romantic Monster window end", -1, -1).trim() !== ""),
     () => 0,
     (options: EmbezzlerFightOptions) => {
       const location =
