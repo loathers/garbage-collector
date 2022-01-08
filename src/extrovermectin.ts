8260162d0d3919e3e104b4370b4583d7f445320f
Skaazi <lawman_mystic_0j@icloud.com>
2022-01-08 10:41:50 -0700

    If no crystal ball, don't need long banishes to initialize dire warren

--- src/extrovermectin.ts
+++ src/extrovermectin.ts
@@ -173,9 +173,11 @@ function initializeCrates(): void {
 }
 
 function initializeDireWarren(): void {
-  const banish = $items`human musk, tryptophan dart, Daily Affirmation: Be a Mind Master`.sort(
-    (a, b) => mallPrice(a) - mallPrice(b)
-  )[0];
+  const longBanishes = $items`human musk, tryptophan dart, Daily Affirmation: Be a Mind Master`;
+  const banishOptions = have($item`miniature crystal ball`)
+    ? longBanishes
+    : [...$items`Louder Than Bomb, tennis ball`, ...longBanishes];
+  const banish = banishOptions.sort((a, b) => mallPrice(a) - mallPrice(b))[0];
   retrieveItem(1, banish);
   do {
     adventureMacro(
