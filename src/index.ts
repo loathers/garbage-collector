3477b4c1c3adda00dc95c466e31dddd5463a98a7
horrible little slime <69secret69email69@gmail.com>
2022-01-10 19:03:28 -0500

    bump version again

--- src/index.ts
+++ src/index.ts
@@ -233,7 +233,7 @@ export function canContinue(): boolean {
 }
 
 export function main(argString = ""): void {
-  sinceKolmafiaRevision(26104);
+  sinceKolmafiaRevision(26108);
   print(`${process.env.GITHUB_REPOSITORY}@${process.env.GITHUB_SHA}`);
   const forbiddenStores = property.getString("forbiddenStores").split(",");
   if (!forbiddenStores.includes("3408540")) {
