a1e84efa00d1410229e0f6e2e92efa30df09fae1
horrible little slime <69secret69email69@gmail.com>
2022-01-20 23:23:54 -0500

    do the thing we want instead of the opposite

--- src/extrovermectin.ts
+++ src/extrovermectin.ts
@@ -105,7 +105,7 @@ export function saberCrateIfDesired(): void {
           .abort()
       );
     } while (
-      ![
+      [
         "Puttin' it on Wax",
         "Wooof! Wooooooof!",
         "Playing Fetch*",
