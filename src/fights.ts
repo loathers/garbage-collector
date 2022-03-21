377612d716773c0ce961b8c60780c82a6c0bb709
horrible little slime <69secret69email69@gmail.com>
2022-03-21 19:46:00 -0400

    reduce use of triple size

--- src/fights.ts
+++ src/fights.ts
@@ -1902,7 +1902,8 @@ function deliverThesis(): void {
   if (
     have($item`Powerful Glove`) &&
     !have($effect`Triple-Sized`) &&
-    get("_powerfulGloveBatteryPowerUsed") <= 95
+    get("_powerfulGloveBatteryPowerUsed") <= 95 &&
+    get("_powerfulGloveBatteryPowerUsed") % 10 === 5
   ) {
     cliExecute("checkpoint");
     equip($slot`acc1`, $item`Powerful Glove`);
