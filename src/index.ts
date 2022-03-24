f3e90b5d2bea990ae5017a44b51e4ac9c1013af5
horrible little slime <69secret69email69@gmail.com>
2022-03-23 21:54:42 -0400

    remove mpa cap

--- src/index.ts
+++ src/index.ts
@@ -295,11 +295,6 @@ export function main(argString = ""): void {
       "valueOfAdventure"
     )}, which is too low for barf farming to be worthwhile. If you forgot to set it, use "set valueOfAdventure = XXXX" to set it to your marginal turn meat value.`;
   }
-  if (get("valueOfAdventure") >= 10000) {
-    throw `Your valueOfAdventure is set to ${get(
-      "valueOfAdventure"
-    )}, which is definitely incorrect. Please set it to your reliable marginal turn value.`;
-  }
 
   const args = argString.split(" ");
   for (const arg of args) {
