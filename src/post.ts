0e5bf5c9f59c087a8c8eb941fe98cea9db7262c4
horrible little slime <69secret69email69@gmail.com>
2022-05-01 10:30:34 -0400

    remove throw from postCombatActions

--- src/post.ts
+++ src/post.ts
@@ -76,7 +76,6 @@ export default function postCombatActions(skipDiet = false): void {
   if (!skipDiet) horseradish();
   coldMedicineCabinet();
   safeInterrupt();
-  if (!myAdventures()) throw new Error("Out of adventures!");
   safeRestore();
   updateMallPrices();
 }
