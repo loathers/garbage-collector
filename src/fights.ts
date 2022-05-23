0e4ad9231a43002f84d9ff3c9aa9737de1e837e2
neil <78829653+horrible-little-slime@users.noreply.github.com>
2022-05-22 22:05:05 -0400

    Thesis smarter not harder (#918)
    * don't thesis during embezzlers, thesis at EOD
    
    * reserve an NEP turn
    
    * eat sausages and thesis even if we save turns

--- src/fights.ts
+++ src/fights.ts
@@ -417,14 +417,6 @@ export function dailyFights(): void {
           postCombatActions();
           const predictedNextFight = getNextEmbezzlerFight();
           if (!predictedNextFight?.draggable) doSausage();
-          // Check in case our prof gained enough exp during the profchain
-          if (
-            thesisReady() &&
-            get("beGregariousFightsLeft") <= 0 &&
-            predictedNextFight?.name !== "Orb Prediction"
-          ) {
-            deliverThesis();
-          }
           doGhost();
           startWandererCounter();
         }
@@ -485,7 +477,6 @@ export function dailyFights(): void {
 
         nextFight = getNextEmbezzlerFight();
 
-        // try to deliver the thesis
         const romanticMonsterImpossible =
           Counter.get("Romantic Monster Window end") === Infinity ||
           (Counter.get("Romantic Monster Window begin") > 0 &&
@@ -493,14 +484,6 @@ export function dailyFights(): void {
           get("_romanticFightsLeft") <= 0;
         if (romanticMonsterImpossible && (!nextFight || !nextFight.draggable)) {
           doSausage();
-          // Check in case our prof gained enough exp during the profchains
-          if (
-            thesisReady() &&
-            get("beGregariousFightsLeft") <= 0 &&
-            nextFight?.name !== "Orb Prediction"
-          ) {
-            deliverThesis();
-          }
           yachtzee();
         }
         doGhost();
@@ -592,7 +575,7 @@ class FreeFight {
       withMacro(Macro.basicCombat(), this.run);
       postCombatActions();
       // Slot in our Professor Thesis if it's become available
-      if (thesisReady() && !have($effect`Feeling Lost`)) deliverThesis();
+      if (!have($effect`Feeling Lost`)) deliverThesisIfAble();
     }
   }
 }
@@ -1269,7 +1252,7 @@ const freeFightSources = [
   new FreeFight(
     () =>
       get("neverendingPartyAlways") && questStep("_questPartyFair") < 999
-        ? clamp(10 - get("_neverendingPartyFreeTurns"), 0, 10)
+        ? clamp(10 - get("_neverendingPartyFreeTurns") - (get("_thesisDelivered") ? 0 : 1), 0, 10)
         : 0,
     () => {
       const constructedMacro = Macro.tryHaveSkill($skill`Feel Pride`).step(Macro.load());
@@ -1925,7 +1908,8 @@ function thesisReady(): boolean {
   );
 }
 
-function deliverThesis(): void {
+export function deliverThesisIfAble(): void {
+  if (!thesisReady()) return;
   const thesisInNEP =
     (get("neverendingPartyAlways") || get("_neverEndingPartyToday")) &&
     questStep("_questPartyFair") < 999;
