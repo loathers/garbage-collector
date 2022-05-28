0e834dacf2c1a2b8a17e2f8596ec9edf22c4c801
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2022-05-28 12:54:43 +0800

    Potentially use Uncle Gator's instead of Barf for jelly fishing
    This ensures that we don't hit a turn-taking NC

--- src/fights.ts
+++ src/fights.ts
@@ -660,7 +660,7 @@ const pygmyMacro = Macro.if_(
 
 function getStenchLocation() {
   return (
-    $locations`Barf Mountain, The Hippy Camp (Bombed Back to the Stone Age), The Dark and Spooky Swamp`.find(
+    $locations`Uncle Gator's Country Fun-Time Liquid Waste Sluice, Barf Mountain, The Hippy Camp (Bombed Back to the Stone Age), The Dark and Spooky Swamp`.find(
       (l) => canAdv(l, false)
     ) || $location`none`
   );
