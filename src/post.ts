ba8975f6da75d877249df2e1706b5bf4ee42395e
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2022-06-01 20:34:19 +0800

    Fixed not running pantsgiving diet after yachtzee chaining

--- src/post.ts
+++ src/post.ts
@@ -50,10 +50,8 @@ function coldMedicineCabinet(): void {
 function horseradish(): void {
   if (
     getRemainingStomach() > 0 &&
-    !(
-      globalOptions.noDiet ||
-      (globalOptions.yachtzeeChain && !get(`"_garboYachtzeeChainCompleted", false`))
-    )
+    !globalOptions.noDiet &&
+    (!globalOptions.yachtzeeChain || get(`"_garboYachtzeeChainCompleted"`, false))
   ) {
     consumeDiet(computeDiet().pantsgiving(), "PANTSGIVING");
   }
