464a414cc9b1c2adf6bbc627dfe382eb75c4611d
horrible little slime <69secret69email69@gmail.com>
2022-06-08 12:07:00 -0400

    update eslint plugin & kolmafia  version req

--- src/index.ts
+++ src/index.ts
@@ -310,7 +310,7 @@ export function canContinue(): boolean {
 }
 
 export function main(argString = ""): void {
-  sinceKolmafiaRevision(26321);
+  sinceKolmafiaRevision(26482);
   print(`${process.env.GITHUB_REPOSITORY}@${process.env.GITHUB_SHA}`);
   const forbiddenStores = property.getString("forbiddenStores").split(",");
   if (!forbiddenStores.includes("3408540")) {
