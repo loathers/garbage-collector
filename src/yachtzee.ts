0f638db2300fa3ed2a0d864e7be5291a2f85c536
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2022-06-22 20:32:12 +0800

    Clean things up by putting things in functions

--- src/yachtzee.ts
+++ src/yachtzee.ts
@@ -1003,6 +1003,17 @@ function yachtzeePotionSetup(yachtzeeTurns: number, simOnly?: boolean): number {
   return totalProfits;
 }
 
+function bestFamUnderwaterGear(fam: Familiar): Item {
+  // Returns best familiar gear for yachtzee chaining
+  return fam.underwater || have($effect`Driving Waterproofly`) || have($effect`Wet Willied`)
+    ? have($item`amulet coin`)
+      ? $item`amulet coin`
+      : $item`filthy child leash`
+    : have($item`das boot`)
+    ? $item`das boot`
+    : $item`little bitty bathysphere`;
+}
+
 export function bestYachtzeeFamiliar(): Familiar {
   const haveUnderwaterFamEquipment = familiarWaterBreathingEquipment.some((item) => have(item));
   const famWt =
@@ -1021,47 +1032,16 @@ export function bestYachtzeeFamiliar(): Familiar {
     )
     .sort(
       (left, right) =>
-        numericModifier(
-          right,
-          "meat drop",
-          famWt,
-          right.underwater || have($effect`Driving Waterproofly`) || have($effect`Wet Willied`)
-            ? have($item`amulet coin`)
-              ? $item`amulet coin`
-              : $item`filthy child leash`
-            : have($item`das boot`)
-            ? $item`das boot`
-            : $item`little bitty bathysphere`
-        ) -
-        numericModifier(
-          left,
-          "meat drop",
-          famWt,
-          left.underwater || have($effect`Driving Waterproofly`) || have($effect`Wet Willied`)
-            ? have($item`amulet coin`)
-              ? $item`amulet coin`
-              : $item`filthy child leash`
-            : have($item`das boot`)
-            ? $item`das boot`
-            : $item`little bitty bathysphere`
-        )
+        numericModifier(right, "Meat Drop", famWt, bestFamUnderwaterGear(right)) -
+        numericModifier(left, "Meat Drop", famWt, bestFamUnderwaterGear(left))
     );
 
   print(`Familiar bonus meat%:`, "blue");
   sortedUnderwaterFamiliars.forEach((fam) => {
     print(
-      `${fam} (${numericModifier(
-        fam,
-        "meat drop",
-        famWt,
-        fam.underwater || have($effect`Driving Waterproofly`) || have($effect`Wet Willied`)
-          ? have($item`amulet coin`)
-            ? $item`amulet coin`
-            : $item`filthy child leash`
-          : have($item`das boot`)
-          ? $item`das boot`
-          : $item`little bitty bathysphere`
-      ).toFixed(2)}%)`,
+      `${fam} (${numericModifier(fam, "Meat Drop", famWt, bestFamUnderwaterGear(fam)).toFixed(
+        2
+      )}%)`,
       "blue"
     );
   });
