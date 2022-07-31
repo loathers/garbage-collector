17f31cafc4f154a872275d072718a09fbf6ca676
Kiwi Tokoeka <82226562+Tokoeka@users.noreply.github.com>
2022-07-31 15:31:51 +1200

    dont consider astral pilsners as "best consumable" (#1021)
    Co-authored-by: Tokoeka <nzgamer666@gmail.com>

--- src/fights.ts
+++ src/fights.ts
@@ -1548,7 +1548,7 @@ const freeRunFightSources = [
         1209: 2, // enter the gallery at Upscale Midnight
         1214: 1, // get High-End ginger wine
       });
-      const best = bestConsumable("booze", $item`high-end ginger wine`);
+      const best = bestConsumable("booze", $items`high-end ginger wine, astral pilsner`);
       const gingerWineValue =
         (0.5 * 30 * (baseMeat + 750) +
           getAverageAdventures($item`high-end ginger wine`) * get("valueOfAdventure")) /
