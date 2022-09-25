99c30ad9e31fe8c82e39322cd3d8aa8f6dd4a68b
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2022-09-25 23:30:57 +0800

    Add opportunity cost of underwater breathing equipment to wishes

--- src/yachtzee/fishy.ts
+++ src/yachtzee/fishy.ts
@@ -165,7 +165,7 @@ export function optimizeForFishy(yachtzeeTurns: number, setup?: boolean): number
     {
       name: "Pocket Wish",
       turns: 20 + (haveFishyPipe ? 10 : 0),
-      cost: mallPrice($item`pocket wish`),
+      cost: mallPrice($item`pocket wish`) + bestWaterBreathingEquipment.cost,
       action: () => {
         acquire(1, $item`pocket wish`, 1.2 * mallPrice($item`pocket wish`));
         if (!have($item`pocket wish`)) {
@@ -180,7 +180,7 @@ export function optimizeForFishy(yachtzeeTurns: number, setup?: boolean): number
     {
       name: "2x Pocket Wish",
       turns: 40 + (haveFishyPipe ? 10 : 0),
-      cost: 2 * mallPrice($item`pocket wish`),
+      cost: 2 * mallPrice($item`pocket wish`) + bestWaterBreathingEquipment.cost,
       action: () => {
         acquire(2, $item`pocket wish`, 1.2 * mallPrice($item`pocket wish`));
         if (availableAmount($item`pocket wish`) < 2) {
@@ -229,7 +229,7 @@ export function optimizeForFishy(yachtzeeTurns: number, setup?: boolean): number
     {
       name: "Just Fishy Pipe",
       turns: 10,
-      cost: haveFishyPipe ? 0 : Infinity,
+      cost: haveFishyPipe ? bestWaterBreathingEquipment.cost : Infinity,
       action: () => {
         if (haveFishyPipe && haveEffect($effect`Fishy`) < yachtzeeTurns) use(1, $item`fishy pipe`);
       },
