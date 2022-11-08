0938879e950f98241cb131f7ff334a350582506a
Skaazi <lawman_mystic_0j@icloud.com>
2022-11-07 20:32:58 -0700

    Only value banishers that are not preempted by skills

--- src/fights.ts
+++ src/fights.ts
@@ -623,15 +623,35 @@ class FreeRunFight extends FreeFight {
   }
 }
 
-const pygmyMacro = Macro.if_(
-  $monster`pygmy bowler`,
-  Macro.trySkill($skill`Snokebomb`).item($item`Louder Than Bomb`)
-)
-  .if_(
-    $monster`pygmy orderlies`,
-    Macro.trySkill($skill`Feel Hatred`).item($item`divine champagne popper`)
+const pygmyBanishHandlers = [
+  {
+    pygmy: $monster`pygmy bowler`,
+    skill: $skill`Snokebomb`,
+    check: "_snokebombUsed",
+    limit: 3,
+    item: $item`Louder Than Bomb`,
+  },
+  {
+    pygmy: $monster`pygmy orderlies`,
+    skill: $skill`Feel Hatred`,
+    check: "_feelHatredUsed",
+    limit: 3,
+    item: $item`divine champagne popper`,
+  },
+  {
+    pygmy: $monster`pygmy janitor`,
+    skill: undefined,
+    check: undefined,
+    limit: undefined,
+    item: $item`tennis ball`,
+  },
+] as const;
+
+const pygmyMacro = Macro.step(
+  ...pygmyBanishHandlers.map(({ pygmy, skill, item }) =>
+    Macro.if_(pygmy, skill ? Macro.trySkill(skill).item(item) : Macro.item(item))
   )
-  .if_($monster`pygmy janitor`, Macro.item($item`tennis ball`))
+)
   .if_($monsters`giant rubber spider, time-spinner prank`, Macro.basicCombat())
   .if_($monster`drunk pygmy`, Macro.trySkill($skill`Extract`).trySkill($skill`Sing Along`))
   .ifHolidayWanderer(Macro.basicCombat())
@@ -964,7 +984,9 @@ const freeFightSources = [
     true,
     {
       cost: () => {
-        const banishers = $items`divine champagne popper, Louder Than Bomb, tennis ball`;
+        const banishers = pygmyBanishHandlers
+          .filter(({ check, limit }) => check && get(check) >= limit)
+          .map(({ item }) => item);
         return retrievePrice($item`Bowl of Scorpions`) + sum(banishers, mallPrice) / 11;
       },
     }
