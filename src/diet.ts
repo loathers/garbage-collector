4ddf9f864d412536cd583c5359841e1d137a844e
Shiverwarp <Lorenz69@gmail.com>
2022-11-08 18:03:03 -0700

    Pantsgiving diet can use special seasoning (#1190)
    * Pantsgiving diet can use special seasoning
    
    * Update src/diet.ts
    
    Co-authored-by: neil <78829653+horrible-little-slime@users.noreply.github.com>
    
    * Lint?
    
    Co-authored-by: neil <78829653+horrible-little-slime@users.noreply.github.com>

--- src/diet.ts
+++ src/diet.ts
@@ -720,7 +720,7 @@ export function computeDiet(): {
           menu().filter(
             (menuItem) =>
               (itemType(menuItem.item) === "food" && menuItem.size === 1) ||
-              [Mayo.flex, Mayo.zapine].includes(menuItem.item)
+              [Mayo.flex, Mayo.zapine, $item`Special Seasoning`].includes(menuItem.item)
           ),
           pantsgivingDietPlanner
         )
