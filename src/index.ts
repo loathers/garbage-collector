b26b25b2d329c60eccfceaa67645a1923b55b6d8
neil <78829653+horrible-little-slime@users.noreply.github.com>
2022-11-18 12:26:16 -0500

    Add oliver's restaurant free fights (#1206)

--- src/index.ts
+++ src/index.ts
@@ -95,7 +95,7 @@ export function canContinue(): boolean {
 }
 
 export function main(argString = ""): void {
-  sinceKolmafiaRevision(26924);
+  sinceKolmafiaRevision(26958);
   checkGithubVersion();
 
   if (get("garbo_autoUserConfirm", false)) {
