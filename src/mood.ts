2fce83d59c40282d5bfcb4137147dac44cb9dd22
horrible little slime <69secret69email69@gmail.com>
2022-11-26 18:05:05 -0500

    Don't be blind, temporarily

--- src/mood.ts
+++ src/mood.ts
@@ -229,8 +229,9 @@ const stings = [
 ];
 const textAlteringEffects = $effects`Can Has Cyborger, Dis Abled, Haiku State of Mind, Just the Best Anapests, O Hai!, Robocamo`;
 const teleportEffects = $effects`Teleportitis, Feeling Lost, Funday!`;
+const otherwiseBadEffects = $effects`Temporary Blindness`;
 export function shrugBadEffects(...exclude: Effect[]): void {
-  [...stings, ...textAlteringEffects, ...teleportEffects].forEach((effect) => {
+  [...stings, ...textAlteringEffects, ...teleportEffects, ...otherwiseBadEffects].forEach((effect) => {
     if (have(effect) && !exclude.includes(effect)) {
       uneffect(effect);
     }
