6838d3cef61f1c0161810e043427ec574af657ff
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2022-12-20 14:31:41 -0600

    don't search for spores every combat (#1225)

--- src/post.ts
+++ src/post.ts
@@ -162,13 +162,15 @@ function stillsuit() {
   }
 }
 
+let funguyWorthIt = true;
 function funguySpores() {
   // Mush-Mouth will drop an expensive mushroom if you do a combat with one turn of it left
   if (
     myLevel() >= 15 && // It applies -100 to all stats, and Level 15 seems to be a reasonable place where you will survive -100 to all stats
     !have($effect`Mush-Mouth`) &&
     (!globalOptions.ascending || myAdventures() > 11) &&
-    get("dinseyRollercoasterNext") // If it were to expire on a rails adventure, you'd waste the cost of the spore. Using it when next turn is rails is easiest way to make sure it won't
+    get("dinseyRollercoasterNext") && // If it were to expire on a rails adventure, you'd waste the cost of the spore. Using it when next turn is rails is easiest way to make sure it won't
+    funguyWorthIt
   ) {
     // According to wiki, it has a 75% chance of being a stat mushroom and 25% chance of being another mushroom
     const value =
@@ -182,7 +184,7 @@ function funguySpores() {
         );
     if (acquire(1, $item`Fun-Guy spore`, value, false) > 0) {
       use($item`Fun-Guy spore`);
-    }
+    } else funguyWorthIt = false;
   }
 }
 
