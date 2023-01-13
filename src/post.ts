6c6d51c3f8abff05898a2891d761223a156a5cca
horrible little slime <69secret69email69@gmail.com>
2023-01-13 13:31:10 -0500

    Fix imports

--- src/post.ts
+++ src/post.ts
@@ -32,6 +32,7 @@ import { computeDiet, consumeDiet } from "./diet";
 import {
   bestJuneCleaverOption,
   juneCleaverChoiceValues,
+  maxBy,
   safeInterrupt,
   safeRestore,
   setChoice,
@@ -41,40 +42,6 @@ import { teleportEffects } from "./mood";
 import { garboAverageValue, garboValue, sessionSinceStart } from "./session";
 import { estimatedTurns } from "./turns";
 import handleWorkshed from "./workshed";
-
-function coldMedicineCabinet(): void {
-  if (getWorkshed() !== $item`cold medicine cabinet`) return;
-
-  if (
-    property.getNumber("_coldMedicineConsults") >= 5 ||
-    property.getNumber("_nextColdMedicineConsult") > totalTurnsPlayed()
-  ) {
-    return;
-  }
-  const options = visitUrl("campground.php?action=workshed");
-  let i = 0;
-  let match;
-  const regexp = /descitem\((\d+)\)/g;
-  const itemChoices = new Map<Item, number>();
-  if (!globalOptions.nobarf) {
-    // if spending turns at barf, we probably will be able to get an extro so always consider it
-    itemChoices.set($item`Extrovermectin™`, -1);
-  }
-
-  while ((match = regexp.exec(options)) !== null) {
-    i++;
-    const item = descToItem(match[1]);
-    itemChoices.set(item, i);
-  }
-
-  const bestItem = maxBy([...itemChoices.keys()], garboValue);
-  const bestChoice = itemChoices.get(bestItem);
-  if (bestChoice && bestChoice > 0) {
-    visitUrl("campground.php?action=workshed");
-    runChoice(bestChoice);
-  }
-  if (handlingChoice()) visitUrl("main.php");
-}
 
 function fillPantsgivingFullness(): void {
   if (
