04fe51a71dd230d3880bf8f8483c8a38dd5c23d5
Sam Gaus <sam@gaus.co.uk>
2023-02-13 09:25:54 +0000

    Update dependencies

--- src/tasks/dailyItems.ts
+++ src/tasks/dailyItems.ts
@@ -304,11 +304,8 @@ export const DailyItemTasks: Task[] = [
     },
     {
       name: "Learn About Bugs",
-      // eslint-disable-next-line libram/verify-constants
       ready: () => have($item`S.I.T. Course Completion Certificate`),
-      // eslint-disable-next-line libram/verify-constants
       completed: () => get("_sitCourseCompleted", true) || have($skill`Insectologist`),
-      // eslint-disable-next-line libram/verify-constants
       do: () => use($item`S.I.T. Course Completion Certificate`),
       choices: { [1494]: 2 },
     },
