e288c45c5da4090ab866ec2bb7d2633f2db0a9e0
horrible little slime <69secret69email69@gmail.com>
2023-04-23 18:51:39 -0400

    don't wrap individual Potion.meatDrop()s in `withLocation`

--- src/potions.ts
+++ src/potions.ts
@@ -218,11 +218,9 @@ export class Potion {
   }
 
   meatDrop(): number {
-    return withLocation(
-      $location.none,
-      () =>
-        getModifier("Meat Drop", this.effect()) +
-        2 * (usingPurse() ? getModifier("Smithsness", this.effect()) : 0)
+    return (
+      getModifier("Meat Drop", this.effect()) +
+      2 * (usingPurse() ? getModifier("Smithsness", this.effect()) : 0)
     );
   }
 
@@ -615,67 +613,70 @@ export function potionSetupCompleted(): boolean {
 export function potionSetup(embezzlersOnly: boolean): void {
   // TODO: Count PYEC.
   // TODO: Count free fights (25 meat each for most).
-  const embezzlers = embezzlerCount();
+  withLocation($location.none, () => {
+    const embezzlers = embezzlerCount();
 
-  if (have($item`Eight Days a Week Pill Keeper`) && !get("_freePillKeeperUsed")) {
-    const possibleDoublingPotions = doublingPotions(embezzlers);
-    const bestPotion = possibleDoublingPotions.length > 0 ? possibleDoublingPotions[0] : undefined;
-    if (bestPotion && bestPotion.doubleDuration().net(embezzlers) > pillkeeperOpportunityCost()) {
-      print(`Determined that ${bestPotion.potion} was the best potion to double`, HIGHLIGHT);
-      cliExecute("pillkeeper extend");
-      bestPotion.acquire(1, bestPotion.potion, bestPotion.doubleDuration().gross(embezzlers));
-      bestPotion.use(1);
+    if (have($item`Eight Days a Week Pill Keeper`) && !get("_freePillKeeperUsed")) {
+      const possibleDoublingPotions = doublingPotions(embezzlers);
+      const bestPotion =
+        possibleDoublingPotions.length > 0 ? possibleDoublingPotions[0] : undefined;
+      if (bestPotion && bestPotion.doubleDuration().net(embezzlers) > pillkeeperOpportunityCost()) {
+        print(`Determined that ${bestPotion.potion} was the best potion to double`, HIGHLIGHT);
+        cliExecute("pillkeeper extend");
+        bestPotion.acquire(1, bestPotion.potion, bestPotion.doubleDuration().gross(embezzlers));
+        bestPotion.use(1);
+      }
     }
-  }
 
-  // Only test potions which are reasonably close to being profitable using historical price.
-  const testPotions = farmingPotions.filter(
-    (potion) => potion.gross(embezzlers) / potion.price(true) > 0.5
-  );
-  const nonWishTestPotions = testPotions.filter((potion) => potion.potion !== $item`pocket wish`);
-  nonWishTestPotions.sort((a, b) => b.net(embezzlers) - a.net(embezzlers));
+    // Only test potions which are reasonably close to being profitable using historical price.
+    const testPotions = farmingPotions.filter(
+      (potion) => potion.gross(embezzlers) / potion.price(true) > 0.5
+    );
+    const nonWishTestPotions = testPotions.filter((potion) => potion.potion !== $item`pocket wish`);
+    nonWishTestPotions.sort((a, b) => b.net(embezzlers) - a.net(embezzlers));
 
-  const excludedEffects = new Set<Effect>();
-  for (const effect of getActiveEffects()) {
-    for (const excluded of mutuallyExclusive.get(effect) ?? []) {
-      excludedEffects.add(excluded);
-    }
-  }
-
-  for (const potion of nonWishTestPotions) {
-    const effect = potion.effect();
-    if (!excludedEffects.has(effect) && useAsValuable(potion, embezzlers, embezzlersOnly) > 0) {
+    const excludedEffects = new Set<Effect>();
+    for (const effect of getActiveEffects()) {
       for (const excluded of mutuallyExclusive.get(effect) ?? []) {
         excludedEffects.add(excluded);
       }
     }
-  }
 
-  usePawWishes((potion) => {
-    const value = potion.value(embezzlers);
-    return value.length > 0
-      ? maxBy(value, ({ quantity, value }) => (quantity > 0 ? value : 0)).value
-      : 0;
+    for (const potion of nonWishTestPotions) {
+      const effect = potion.effect();
+      if (!excludedEffects.has(effect) && useAsValuable(potion, embezzlers, embezzlersOnly) > 0) {
+        for (const excluded of mutuallyExclusive.get(effect) ?? []) {
+          excludedEffects.add(excluded);
+        }
+      }
+    }
+
+    usePawWishes((potion) => {
+      const value = potion.value(embezzlers);
+      return value.length > 0
+        ? maxBy(value, ({ quantity, value }) => (quantity > 0 ? value : 0)).value
+        : 0;
+    });
+
+    const wishTestPotions = testPotions.filter((potion) => potion.potion === $item`pocket wish`);
+    wishTestPotions.sort((a, b) => b.net(embezzlers) - a.net(embezzlers));
+
+    for (const potion of wishTestPotions) {
+      const effect = potion.effect();
+      if (
+        !excludedEffects.has(effect) &&
+        !failedWishes.includes(effect) &&
+        useAsValuable(potion, embezzlers, embezzlersOnly) > 0
+      ) {
+        for (const excluded of mutuallyExclusive.get(effect) ?? []) {
+          excludedEffects.add(excluded);
+        }
+      }
+    }
+
+    variableMeatPotionsSetup(0, embezzlers);
+    completedPotionSetup = true;
   });
-
-  const wishTestPotions = testPotions.filter((potion) => potion.potion === $item`pocket wish`);
-  wishTestPotions.sort((a, b) => b.net(embezzlers) - a.net(embezzlers));
-
-  for (const potion of wishTestPotions) {
-    const effect = potion.effect();
-    if (
-      !excludedEffects.has(effect) &&
-      !failedWishes.includes(effect) &&
-      useAsValuable(potion, embezzlers, embezzlersOnly) > 0
-    ) {
-      for (const excluded of mutuallyExclusive.get(effect) ?? []) {
-        excludedEffects.add(excluded);
-      }
-    }
-  }
-
-  variableMeatPotionsSetup(0, embezzlers);
-  completedPotionSetup = true;
 }
 
 /**
