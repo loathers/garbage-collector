ec461219dba5c1ff58adacdbf816ef31305b5995
Pantocyclus <98746573+Pantocyclus@users.noreply.github.com>
2023-04-28 05:56:43 +0800

    retrieve_price() fix (#1355)
    * Update defaultPriceFunction to consider npcPrice
    
    * Also re-align sweat before diet (and outfit equipping)
    
    * Handle unpurchaseable items
    
    * Use examine
    
    * Don't exceed MAX_SAFE_INTEGER

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,7 @@ import {
   $skill,
   $slots,
   Clan,
+  examine,
   get,
   getFoldGroup,
   have,
@@ -241,6 +242,9 @@ export function main(argString = ""): void {
 
   const completedProperty = "_garboCompleted";
   set(completedProperty, "");
+
+  // re-align sweat (useful for diet and outfit)
+  examine($item`designer sweatpants`);
 
   startSession();
   if (!globalOptions.nobarf && !globalOptions.simdiet) {
