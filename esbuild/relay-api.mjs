a5507274901ae8bbd0c2f2db3731bcaa1383f32e
Joe Kirchoff <joe.kirchoff@epicgames.com>
2023-07-06 13:27:29 -0700

    Update relay-api.mjs

--- esbuild/relay-api.mjs
+++ esbuild/relay-api.mjs
@@ -2,8 +2,14 @@ import babel from "esbuild-plugin-babel";
 
 export default {
   entryPoints: ["src/relay_garbo.ts"],
-  outdir: "KoLmafia/relay",
+  bundle: true,
   platform: "node",
   target: "rhino1.7.14",
+  external: ["kolmafia"],
   plugins: [babel()],
+  outdir: "KoLmafia/relay",
+  define: {
+    "process.env.GITHUB_SHA": '"CustomBuild"',
+    "process.env.GITHUB_REPOSITORY": '"CustomBuild"',
+  },
 };
