e59095654d060dc65a9dcdb7246723af579398a7
Patrick Stalcup <legomaniac0117@gmail.com>
2023-09-05 08:38:47 -0400

    Move type and array of banishes outside of function definition

--- src/extrovermectin.ts
+++ src/extrovermectin.ts
@@ -276,68 +276,71 @@ function getClub() {
   return availableClub;
 }
 
+const MAX_BANISH_PRICE = 100000; // price of nanobrawny
+
+type Banish = {
+  name: string;
+  macro: Macro;
+  available: () => boolean;
+  price?: () => number;
+  prepare?: () => void;
+};
+
+const combatItem = (item: Item, maxPrice?: number): Banish => ({
+  name: `${item}`,
+  available: () => mallPrice(item) < (maxPrice ?? MAX_BANISH_PRICE),
+  macro: Macro.item(item),
+  price: () => mallPrice(item),
+  prepare: () => acquire(1, item, maxPrice ?? MAX_BANISH_PRICE), // put a sanity ceiling of 50k on the banish
+});
+
+const longBanishes: Banish[] = [
+  combatItem($item`human musk`),
+  combatItem($item`tryptophan dart`),
+  combatItem($item`Daily Affirmation: Be a Mind Master`),
+  {
+    name: "Batter Up!",
+    available: () => myFury() > 5 && have($skill`Batter Up!`),
+    macro: Macro.skill($skill`Batter Up!`),
+    prepare: () => {
+      const club = getClub();
+      new Requirement(["100 Monster Level"], {
+        preventEquip: $items`carnivorous potted plant`,
+        forceEquip: [club],
+      }).maximize();
+    },
+  },
+  {
+    name: "Mafia Middle Finger Ring",
+    available: () => have($item`mafia middle finger ring`) && !get("_mafiaMiddleFingerRingUsed"),
+    macro: Macro.skill($skill`Show them your ring`),
+    prepare: () => {
+      new Requirement(["100 Monster Level"], {
+        preventEquip: $items`carnivorous potted plant`,
+        forceEquip: [$item`mafia middle finger ring`],
+      }).maximize();
+    },
+  },
+  {
+    name: "Nanobrawny",
+    available: () => true,
+    price: () => mallPrice($item`pocket wish`) * 2, // could be 3 if you are unlucky
+    macro: Macro.skill($skill`Unleash Nanites`),
+    prepare: () => {
+      while (haveEffect($effect`Nanobrawny`) < 40) {
+        acquire(1, $item`pocket wish`, 50000);
+        cliExecute(`genie effect Nanobrawny`);
+      }
+    },
+  },
+];
+
+const shortBanishes = [
+  combatItem($item`Louder Than Bomb`, 10000),
+  combatItem($item`tennis ball`, 10000),
+];
+
 function banishBunny(): void {
-  const maxBanishPrice = 100000; // price of nanobrawny
-
-  type Banish = {
-    name: string;
-    macro: Macro;
-    available: () => boolean;
-    price?: () => number;
-    prepare?: () => void;
-  };
-
-  const combatItem = (item: Item): Banish => ({
-    name: `${item}`,
-    available: () => mallPrice(item) < maxBanishPrice,
-    macro: Macro.item(item),
-    price: () => mallPrice(item),
-    prepare: () => acquire(1, item, maxBanishPrice), // put a sanity ceiling of 50k on the banish
-  });
-
-  const longBanishes: Banish[] = [
-    combatItem($item`human musk`),
-    combatItem($item`tryptophan dart`),
-    combatItem($item`Daily Affirmation: Be a Mind Master`),
-    {
-      name: "Batter Up!",
-      available: () => myFury() > 5 && have($skill`Batter Up!`),
-      macro: Macro.skill($skill`Batter Up!`),
-      prepare: () => {
-        const club = getClub();
-        new Requirement(["100 Monster Level"], {
-          preventEquip: $items`carnivorous potted plant`,
-          forceEquip: [club],
-        }).maximize();
-      },
-    },
-    {
-      name: "Mafia Middle Finger Ring",
-      available: () => have($item`mafia middle finger ring`) && !get("_mafiaMiddleFingerRingUsed"),
-      macro: Macro.skill($skill`Show them your ring`),
-      prepare: () => {
-        new Requirement(["100 Monster Level"], {
-          preventEquip: $items`carnivorous potted plant`,
-          forceEquip: [$item`mafia middle finger ring`],
-        }).maximize();
-      },
-    },
-    {
-      name: "Nanobrawny",
-      available: () => true,
-      price: () => mallPrice($item`pocket wish`) * 2, // could be 3 if you are unlucky
-      macro: Macro.skill($skill`Unleash Nanites`),
-      prepare: () => {
-        while (haveEffect($effect`Nanobrawny`) < 40) {
-          acquire(1, $item`pocket wish`, 50000);
-          cliExecute(`genie effect Nanobrawny`);
-        }
-      },
-    },
-  ];
-
-  const shortBanishes = [combatItem($item`Louder Than Bomb`), combatItem($item`tennis ball`)];
-
   const banishes = [
     ...longBanishes,
     ...(!have($item`miniature crystal ball`) ? shortBanishes : []),
