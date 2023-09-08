404e516f2040cbbd66668b43ecd2908400687d80
Shiverwarp <Lorenz69@gmail.com>
2023-09-07 20:48:06 -0600

    Don't need to export

--- src/embezzler.ts
+++ src/embezzler.ts
@@ -624,7 +624,7 @@ export const gregFights = (
   ];
 };
 
-export const gregLikeSources = [
+const gregLikeSources = [
   ...gregFights(
     "Be Gregarious",
     () => true, // we can always use extrovermectin
