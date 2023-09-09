21003f5da00a1cd1d281163c65221533a8127737
Shiverwarp <Lorenz69@gmail.com>
2023-09-08 19:21:20 -0600

    Check ice house first

--- src/tasks/daily.ts
+++ src/tasks/daily.ts
@@ -45,6 +45,7 @@ import {
   BeachComb,
   findLeprechaunMultiplier,
   get,
+  getBanishedMonsters,
   getModifier,
   have,
   maxBy,
@@ -370,6 +371,7 @@ export function configureSnojo(): void {
 function determineFreeEmbezzlerBanish(): void {
   if (freeBanishesChecked) return;
   const useFreeBanishes =
+    getBanishedMonsters().get($item`ice house`) !== $monster`fluffy bunny` &&
     gregLikeFightCount() < 120 && // 60 turns of banish from mafia middle finger ring, and 30 x 2 from two snokebombs
     gregLikeFightCount() > 0 &&
     have($item`mafia middle finger ring`) &&
