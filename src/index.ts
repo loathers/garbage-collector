02d4e20af46e5f55d4c3b8908bb9e61864183b0c
Shiverwarp <Lorenz69@gmail.com>
2023-09-10 01:14:46 -0600

    27577 has bugfix for habitats

--- src/index.ts
+++ src/index.ts
@@ -104,7 +104,7 @@ export function canContinue(): boolean {
 }
 
 export function main(argString = ""): void {
-  sinceKolmafiaRevision(27567);
+  sinceKolmafiaRevision(27577);
   checkGithubVersion();
 
   // Hit up main.php to get out of easily escapable choices
