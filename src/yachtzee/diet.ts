b1dcc13e27a3d5dc008c9ff23d9eaa792784259c
Sam Gaus <sam@gaus.co.uk>
2023-09-14 14:16:49 +0100

    We check fullness in this way in another location

--- src/yachtzee/diet.ts
+++ src/yachtzee/diet.ts
@@ -298,7 +298,8 @@ export function executeNextDietStep(stopBeforeJellies?: boolean): void {
           }
           if (
             myFullness() + entry.fullness >
-            fullnessLimit() + (!get("_distentionPillUsed") && have($item`distention pill`) ? 1 : 0)
+            Math.max(fullnessLimit(), myFullness()) +
+              (!get("_distentionPillUsed") && have($item`distention pill`) ? 1 : 0)
           ) {
             throw new Error(`consuming ${entry.name} will exceed our fullness limit`);
           } else if (myInebriety() + entry.drunkenness > inebrietyLimit()) {
