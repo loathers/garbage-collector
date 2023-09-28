3a802567e4b09053be2da95c148b3624819cda81
Patrick Stalcup <legomaniac0117@gmail.com>
2023-09-28 14:32:29 -0400

    Add effect value

--- src/garboWanderer.ts
+++ src/garboWanderer.ts
@@ -1,10 +1,12 @@
-import { Location } from "kolmafia";
+import { Effect, Location } from "kolmafia";
 import { globalOptions } from "./config";
 import { freeFightFamiliarData } from "./familiar/freeFightFamiliar";
 import { estimatedGarboTurns } from "./turns";
 import { WandererManager } from "./wanderer";
-import { get } from "libram";
+import { $item, get } from "libram";
 import { garboValue } from "./garboValue";
+import { Potion } from "./potions";
+import { embezzlerCount } from "./embezzler";
 
 let _wanderer: WandererManager | undefined;
 export function wanderer(): WandererManager {
@@ -13,7 +15,8 @@ export function wanderer(): WandererManager {
       ascend: globalOptions.ascend,
       estimatedTurns: estimatedGarboTurns,
       itemValue: garboValue,
-      effectValue: () => 0,
+      effectValue: (effect: Effect, duration: number) =>
+        new Potion($item.none, { effect, duration }).gross(embezzlerCount()),
       prioritizeCappingGuzzlr: get("garbo_prioritizeCappingGuzzlr", false),
       freeFightExtraValue: (location: Location) =>
         freeFightFamiliarData({ location }).expectedValue,
