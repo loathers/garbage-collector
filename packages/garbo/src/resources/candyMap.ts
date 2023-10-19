bf02f59f1345d8fb42ca42a651ea591ec518aba2
horrible little slime <69secret69email69@gmail.com>
2023-10-19 16:40:06 -0400

    misc fixes

--- packages/garbo/src/resources/candyMap.ts
+++ packages/garbo/src/resources/candyMap.ts
@@ -1,17 +1,9 @@
-import { AcquireItem, Outfit } from "grimoire-kolmafia";
-import {
-  $familiar,
-  $item,
-  get,
-  getSaleValue,
-  have,
-  sum,
-} from "libram";
+import { Outfit } from "grimoire-kolmafia";
+import { $familiar, $item, get, getSaleValue, have, maxBy, sum } from "libram";
 import {
   canEquip,
   getOutfits,
   mallPrice,
-  outfit,
   outfitPieces,
   outfitTreats,
   print,
@@ -23,6 +15,7 @@ import {
 import { GarboTask } from "../tasks/engine";
 import { garboValue } from "../garboValue";
 import { acquire } from "../acquire";
+import { GarboStrategy, Macro } from "../combat";
 
 const HOUSE_NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
@@ -36,13 +29,13 @@ function getBlockHtml(): string {
 function treatValue(outfit: string): number {
   return sum(
     Object.entries(outfitTreats(outfit)),
-    ([candyName, probability]) => probability * garboValue(toItem(candyName))
+    ([candyName, probability]) => probability * garboValue(toItem(candyName)),
   );
 }
 
 export function getTreatOutfit(): string {
   const availableOutfits = getOutfits().filter((name) =>
-    outfitPieces(name).every((piece) => canEquip(piece))
+    outfitPieces(name).every((piece) => canEquip(piece)),
   );
   if (!availableOutfits.length) {
     print("You don't seem to actually have any outfits available, my friend!");
@@ -54,8 +47,9 @@ export function treatOutfit(): Outfit {
   const outfit = new Outfit();
   const pieces = outfitPieces(getTreatOutfit());
   for (const piece of pieces) {
-    if (!outfit.equip(piece))
+    if (!outfit.equip(piece)) {
       print(`Could not equip all pieces of treat outfit: aborted on ${piece}`);
+    }
   }
 
   outfit.equip($familiar`Trick-or-Treating Tot`);
@@ -63,31 +57,34 @@ export function treatOutfit(): Outfit {
 }
 
 export function candyRichBlockValue(): number {
-    const outfitCandyValue = treatValue(getTreatOutfit());
-    const totOutfitCandyMultiplier = have($familiar`Trick-or-Treating Tot`) ? 1.6 : 1;
-    const bowlValue = (1 / 5) * getSaleValue($item`huge bowl of candy`);
-    const prunetsValue = have($familiar`Trick-or-Treating Tot`)
-      ? 4 * 0.2 * getSaleValue($item`Prunets`)
-      : 0;
+  const outfitCandyValue = treatValue(getTreatOutfit());
+  const totOutfitCandyMultiplier = have($familiar`Trick-or-Treating Tot`)
+    ? 1.6
+    : 1;
+  const bowlValue = (1 / 5) * getSaleValue($item`huge bowl of candy`);
+  const prunetsValue = have($familiar`Trick-or-Treating Tot`)
+    ? 4 * 0.2 * getSaleValue($item`Prunets`)
+    : 0;
 
-    const outfitCandyTotal = 3 * outfitCandyValue * totOutfitCandyMultiplier;
-    return (outfitCandyTotal + bowlValue + prunetsValue);
+  const outfitCandyTotal = 3 * outfitCandyValue * totOutfitCandyMultiplier;
+  return outfitCandyTotal + bowlValue + prunetsValue;
 }
 
 function shouldAcquireCandyMap(): boolean {
-  return candyRichBlockValue() < mallPrice($item`map to a candy-rich block`)
+  return candyRichBlockValue() < mallPrice($item`map to a candy-rich block`);
 }
 
 function useCandyMapTask(): GarboTask {
   return {
-  name: "Acquire Candy Map",
-  ready: () => shouldAcquireCandyMap(),
-  completed: () => get("_mapToACandyRichBlockUsed"),
-  do: (): void => {
-    acquire(1, $item`map to a candy-rich block`, candyRichBlockValue() - 1)
-    use($item`map to a candy-rich block`)
-  },
-};
+    name: "Acquire Candy Map",
+    ready: () => shouldAcquireCandyMap(),
+    completed: () => get("_mapToACandyRichBlockUsed"),
+    do: (): void => {
+      acquire(1, $item`map to a candy-rich block`, candyRichBlockValue() - 1);
+      use($item`map to a candy-rich block`);
+    },
+    spendsTurn: false,
+  };
 }
 
 function doCandyTreat(): GarboTask {
@@ -99,17 +96,25 @@ function doCandyTreat(): GarboTask {
       // We do all treat houses in a row as one task for speed reasons
       for (const house of HOUSE_NUMBERS) {
         if (getBlockHtml().match(RegExp(`whichhouse=${house}>[^>]*?house_l`))) {
-          visitUrl(`choice.php?whichchoice=804&option=3&whichhouse=${house}&pwd`);
-        } else if (getBlockHtml().match(RegExp(`whichhouse=${house}>[^>]*?starhouse`))) {
-          visitUrl(`choice.php?whichchoice=804&option=3&whichhouse=${house}&pwd`);
+          visitUrl(
+            `choice.php?whichchoice=804&option=3&whichhouse=${house}&pwd`,
+          );
+        } else if (
+          getBlockHtml().match(RegExp(`whichhouse=${house}>[^>]*?starhouse`))
+        ) {
+          visitUrl(
+            `choice.php?whichchoice=804&option=3&whichhouse=${house}&pwd`,
+          );
           runChoice(2);
           visitUrl(`place.php?whichplace=town&action=town_trickortreat`);
         }
       }
     },
+    spendsTurn: false,
+    combat: new GarboStrategy(Macro.abort()),
   };
 }
 
 export function freeCandyTasks(): GarboTask[] {
-  return [useCandyMapTask() + doCandyTreat()];
+  return [useCandyMapTask(), doCandyTreat()];
 }
