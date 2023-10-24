5c5f3cddd79fdffd455c95c75ef540546fd51c15
horrible little slime <69secret69email69@gmail.com>
2023-10-24 19:31:44 -0400

    support trick or treat combats

--- packages/garbo/src/resources/candyMap.ts
+++ packages/garbo/src/resources/candyMap.ts
@@ -1,5 +1,14 @@
 import { Outfit } from "grimoire-kolmafia";
-import { $familiar, $item, get, getSaleValue, have, maxBy, sum } from "libram";
+import {
+  $familiar,
+  $item,
+  $items,
+  get,
+  getSaleValue,
+  have,
+  maxBy,
+  sum,
+} from "libram";
 import {
   canEquip,
   getOutfits,
@@ -17,15 +26,17 @@ import { GarboTask } from "../tasks/engine";
 import { garboValue } from "../garboValue";
 import { acquire } from "../acquire";
 import { GarboStrategy, Macro } from "../combat";
+import { freeFightOutfit } from "../outfit";
 
 const HOUSE_NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-
+const checkedHousesForTricking: number[] = [];
 let blockHtml = "";
-
 function getBlockHtml(): string {
   blockHtml ||= visitUrl("place.php?whichplace=town&action=town_trickortreat");
   return blockHtml;
 }
+
+const trickHats = $items`invisible bag, witch hat, beholed bedsheet, wolfman mask, pumpkinhead mask, mummy costume`;
 
 function treatValue(outfit: string): number {
   return sum(
@@ -106,22 +117,26 @@ function useCandyMapTask(): GarboTask {
   };
 }
 
+let treated = false;
 function doCandyTreat(): GarboTask {
   return {
     name: "Treat",
-    completed: () =>
-      !get("_mapToACandyRichBlockUsed") || holiday().includes("Halloween"),
+    completed: () => treated,
+    ready: () =>
+      !holiday().includes("Halloween") && get("_mapToACandyRichBlockUsed"),
     outfit: treatOutfit,
     do: (): void => {
       // We do all treat houses in a row as one task for speed reasons
       for (const house of HOUSE_NUMBERS) {
         if (getBlockHtml().match(RegExp(`whichhouse=${house}>[^>]*?house_l`))) {
+          checkedHousesForTricking.push(house);
           visitUrl(
             `choice.php?whichchoice=804&option=3&whichhouse=${house}&pwd`,
           );
         } else if (
           getBlockHtml().match(RegExp(`whichhouse=${house}>[^>]*?starhouse`))
         ) {
+          checkedHousesForTricking.push(house);
           visitUrl(
             `choice.php?whichchoice=804&option=3&whichhouse=${house}&pwd`,
           );
@@ -129,13 +144,62 @@ function doCandyTreat(): GarboTask {
           visitUrl(`place.php?whichplace=town&action=town_trickortreat`);
         }
       }
+      treated = true;
     },
-    limit: { skip: 1 },
     spendsTurn: false,
     combat: new GarboStrategy(Macro.abort()),
   };
 }
 
-export function candyMapTasks(): GarboTask[] {
-  return [useCandyMapTask(), doCandyTreat()];
+const MAX_HAT_PRICE = 100_000;
+function obtainTrickHat(): GarboTask {
+  return {
+    name: "Obtain Trick Hat",
+    completed: () => trickHats.some((hat) => have(hat)),
+    ready: () => trickHats.some((hat) => mallPrice(hat) < MAX_HAT_PRICE),
+    do: () => {
+      const cheapestHat = maxBy(trickHats, mallPrice, true);
+      acquire(1, cheapestHat, MAX_HAT_PRICE);
+    },
+    spendsTurn: false,
+  };
+}
+
+export function candyMapDailyTasks(): GarboTask[] {
+  return [useCandyMapTask(), doCandyTreat(), obtainTrickHat()];
+}
+
+export function doCandyTrick(): GarboTask {
+  return {
+    name: "Trick",
+    completed: () => checkedHousesForTricking.length >= HOUSE_NUMBERS.length,
+    ready: () =>
+      !holiday().includes("Halloween") &&
+      get("_mapToACandyRichBlockUsed") &&
+      trickHats.some((hat) => have(hat)),
+    do: (): void => {
+      for (const house of HOUSE_NUMBERS) {
+        if (checkedHousesForTricking.includes(house)) continue;
+        checkedHousesForTricking.push(house);
+        if (getBlockHtml().match(RegExp(`whichhouse=${house}>[^>]*?house_d`))) {
+          visitUrl(`place.php?whichplace=town&action=town_trickortreat`);
+          visitUrl(
+            `choice.php?whichchoice=804&option=3&whichhouse=${house}&pwd`,
+          );
+          return;
+        }
+      }
+    },
+    outfit: (): Outfit => {
+      const hat = trickHats.find((hat) => have(hat));
+      if (!hat) {
+        throw new Error(
+          "We thought we had an appropriate hat for tricking, but we did not.",
+        );
+      }
+      return freeFightOutfit({ hat });
+    },
+    combat: new GarboStrategy(() => Macro.basicCombat()),
+    spendsTurn: false,
+  };
 }
