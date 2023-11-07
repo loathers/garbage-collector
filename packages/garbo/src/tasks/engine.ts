0ab7d9880a751b0889c367a4f48fd23b0be728fd
Shiverwarp <Lorenz69@gmail.com>
2023-11-07 16:51:49 -0700

    Do safe restore in prep

--- packages/garbo/src/tasks/engine.ts
+++ packages/garbo/src/tasks/engine.ts
@@ -59,6 +59,11 @@ export class BaseGarboEngine extends Engine<never, GarboTask> {
     }
   }
 
+  prepare(task: GarboTask): void {
+    if ("combat" in task) safeRestore();
+    super.prepare?.(task);
+  }
+
   execute(task: GarboTask): void {
     safeInterrupt();
     const spentTurns = totalTurnsPlayed();
@@ -71,7 +76,6 @@ export class BaseGarboEngine extends Engine<never, GarboTask> {
     ) {
       SourceTerminal.educate([$skill`Extract`, $skill`Duplicate`]);
     }
-    if ("combat" in task) safeRestore();
     super.execute(task);
     if (totalTurnsPlayed() !== spentTurns) {
       if (!undelay(task.spendsTurn)) {
