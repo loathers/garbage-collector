569097a8c758a64876ccac9580eb531fd3c4679d
gausie <sam@gaus.co.uk>
2023-11-29 16:03:14 +0000

    Fix another issue

--- scripts/garbage-collector/garbo.js
+++ scripts/garbage-collector/garbo.js
@@ -30308,7 +30308,7 @@ function checkGithubVersion() {
     var releaseSHA = (_gitBranches$find = gitBranches.find(function(branchInfo) {
       return branchInfo.name === "release";
     })) === null || _gitBranches$find === void 0 || (_gitBranches$find = _gitBranches$find.commit) === null || _gitBranches$find === void 0 ? void 0 : _gitBranches$find.sha;
-    (0, import_kolmafia78.print)("Local Version: ".concat(localSHA, " (built from ").concat("main", "@").concat("95097aa4528fdcdad776e36ebb4be9284b246fd2", ")"));
+    (0, import_kolmafia78.print)("Local Version: ".concat(localSHA, " (built from ").concat("main", "@").concat("84201fbbfa95301e794e361ec9d6c41a7c1a1641", ")"));
     if (releaseSHA === localSHA) {
       (0, import_kolmafia78.print)("Garbo is up to date!", HIGHLIGHT);
     } else if (releaseSHA === void 0) {
