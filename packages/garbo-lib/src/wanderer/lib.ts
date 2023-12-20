1ac0bd91e1fea482811cab1fe6aad72ea42a8a7f
horrible little slime <69secret69email69@gmail.com>
2023-12-20 16:40:18 -0500

    no more wandering in the monorail

--- packages/garbo-lib/src/wanderer/lib.ts
+++ packages/garbo-lib/src/wanderer/lib.ts
@@ -117,7 +117,12 @@ export const UnlockableZones: UnlockableZone[] = [
 export function underwater(location: Location): boolean {
   return location.environment === "underwater";
 }
-const ILLEGAL_PARENTS = ["Clan Basement", "Psychoses", "PirateRealm"];
+const ILLEGAL_PARENTS = [
+  "Clan Basement",
+  "Psychoses",
+  "PirateRealm",
+  "A Monorail Station",
+];
 const ILLEGAL_ZONES = ["The Drip", "Suburbs"];
 const canAdventureOrUnlockSkipList = [
   ...$locations`The Oasis, The Bubblin' Caldera, Barrrney's Barrr, The F'c'le, The Poop Deck, Belowdecks, Madness Bakery, The Secret Government Laboratory, The Dire Warren, Inside the Palindome, The Haiku Dungeon, An Incredibly Strange Place (Bad Trip), An Incredibly Strange Place (Mediocre Trip), An Incredibly Strange Place (Great Trip), El Vibrato Island, The Daily Dungeon, Trick-or-Treating, Seaside Megalopolis, Frat House, Through the Spacegate`,
