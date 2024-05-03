3574799f3f53e89f61067de0077f387f78eda293
Ignose <ihpares@gmail.com>
2024-05-02 20:35:16 -0400

    Small ternary (?) fix

--- packages/garbo/src/resources/chestMimic.ts
+++ packages/garbo/src/resources/chestMimic.ts
@@ -2,7 +2,7 @@ import { $familiar, get } from "libram";
 import { globalOptions } from "../config";
 
 export const mimicExperienceNeeded = () =>
-  50 * (11 - get("_mimicEggsObtained")) + (globalOptions.ascend ? 550 : 0);
+  50 * (11 - get("_mimicEggsObtained")) + (globalOptions.ascend ? 0 : 550);
 
 export function shouldChargeMimic(): boolean {
   /* If we can't make any more eggs tomorrow, don't charge the mimic more */
