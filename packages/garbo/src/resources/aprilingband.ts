c11b84b3a3716b2d0d642553011f8256037a92a4
Shiverwarp <Lorenz69@gmail.com>
2024-05-05 12:46:09 -0600

    Account for how many times we can use each instrument

--- packages/garbo/src/resources/aprilingband.ts
+++ packages/garbo/src/resources/aprilingband.ts
@@ -12,22 +12,28 @@ const instruments: {
   {
     instrument: "Apriling band quad tom",
     value: () =>
-      globalOptions.prefs.valueOfFreeFight +
-      0.02 * garboValue($item`spice melange`),
+      (globalOptions.prefs.valueOfFreeFight +
+        0.02 * garboValue($item`spice melange`)) *
+      3,
   },
   {
     instrument: "Apriling band saxophone",
-    value: () => getBestLuckyAdventure().value(),
+    value: () => getBestLuckyAdventure().value() * 3,
   },
   {
     instrument: "Apriling band piccolo",
     value: () =>
       Math.max(
         0,
-        ...getExperienceFamiliars().map(
-          ({ expectedValue }) => expectedValue / 12,
-        ),
-      ) * 40,
+        ...getExperienceFamiliars().map(({ familiar, expectedValue }) => {
+          const usesAllowed = clamp(
+            Math.floor((400 - familiar.experience) / 40),
+            0,
+            3,
+          );
+          return (expectedValue / 12) * 40 * usesAllowed;
+        }),
+      ),
   },
 ];
 
