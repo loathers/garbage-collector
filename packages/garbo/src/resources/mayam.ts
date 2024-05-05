966cdff1d288e8969740a801f4cbfd62690cef54
Ignose <ihpares@gmail.com>
2024-05-05 17:55:06 -0400

    Just Mayam summons for now

--- packages/garbo/src/resources/mayam.ts
+++ packages/garbo/src/resources/mayam.ts
@@ -1,88 +1,59 @@
 /* eslint-disable libram/verify-constants */
 import { cliExecute, myLevel } from "kolmafia";
-import { $item, clamp, get } from "libram";
+import { $item, clamp, get, maxBy } from "libram";
 import { garboValue } from "../garboValue";
 import { GarboTask } from "../tasks/engine";
 import getExperienceFamiliars from "../familiar/experienceFamiliars";
 import { globalOptions } from "../config";
 
-type MayamSummon = {
+type MayamRingSummon = {
   choice: string;
   value: () => number;
-  type: "resonance" | "ring";
-  ring?: number;
+  ring: number;
 };
-const MAYAM_OPTIONS: MayamSummon[] = [
+
+/* type MayamResonanceSummon = {
+  choice: string;
+  value: () => number;
+  components: string[];
+} */
+
+const yamUses = () => get("_mayamSymbolsUsed");
+
+const MAYAM_RING_OPTIONS: MayamRingSummon[] = [
   {
-    choice: "yam meat eyepatch yam",
-    value: () => 0.05 * 275,
-    type: "resonance",
-  },
-  {
-    choice: "eye yam eyepatch yam",
-    value: () => garboValue($item`Mayam spinach`),
-    type: "resonance",
-  },
-  {
-    choice: "vessel yam cheese explosion",
-    value: () => garboValue($item`stuffed yam stinkbomb`),
-    type: "resonance",
-  },
-  {
-    choice: "yam yam yam explosion",
-    value: () => garboValue($item`thanksgiving bomb`),
-    type: "resonance",
-  },
-  {
-    choice: "yam meat cheese yam",
-    value: () => garboValue($item`yam and swiss`),
-    type: "resonance",
-  },
-  {
-    choice: "yam lightning yam clock",
-    value: () => garboValue($item`yam battery`),
-    type: "resonance",
-  },
-  {
-    choice: "yam1",
+    choice: "yam",
     value: () => garboValue($item`yam`),
-    type: "ring",
     ring: 1,
   },
   {
-    choice: "yam2",
+    choice: "yam",
     value: () => garboValue($item`yam`),
-    type: "ring",
     ring: 2,
   },
   {
-    choice: "yam3",
+    choice: "yam",
     value: () => garboValue($item`yam`),
-    type: "ring",
     ring: 3,
   },
   {
-    choice: "yam4",
+    choice: "yam",
     value: () => garboValue($item`yam`),
-    type: "ring",
     ring: 4,
   },
   {
     choice: "sword",
     value: () => 0,
-    type: "ring",
     ring: 1,
   },
   {
     choice: "eye",
     value: () => 5, // +30% item drop, I don't know how to value that?
-    type: "ring",
     ring: 1,
   },
   {
     choice: "chair",
     value: () => 5, // +5 free rests, I don't know how to value that?
-    type: "ring",
     ring: 1,
   },
   {
@@ -94,61 +65,51 @@ const MAYAM_OPTIONS: MayamSummon[] = [
           ({ expectedValue }) => expectedValue / 12,
         ),
       ) * 100,
-    type: "ring",
     ring: 1,
   },
   {
     choice: "vessel",
     value: () => 5, // How do we value MP regen?
-    type: "ring",
     ring: 1,
   },
   {
     choice: "lightning",
     value: () => 0,
-    type: "ring",
     ring: 2,
   },
   {
     choice: "bottle",
     value: () => 0, // How do we value HP regen?
-    type: "ring",
     ring: 2,
   },
   {
     choice: "meat",
     value: () => clamp(myLevel() * 100, 100, 1500),
-    type: "ring",
     ring: 2,
   },
   {
     choice: "wood",
     value: () => 0,
-    type: "ring",
     ring: 2,
   },
   {
     choice: "wall",
     value: () => 0,
-    type: "ring",
     ring: 3,
   },
   {
     choice: "cheese",
     value: () => garboValue($item`goat cheese`),
-    type: "ring",
     ring: 3,
   },
   {
     choice: "eyepatch",
     value: () => 0,
-    type: "ring",
     ring: 3,
   },
   {
     choice: "explosion",
     value: () => 0,
-    type: "ring",
     ring: 4,
   },
   {
@@ -158,66 +119,88 @@ const MAYAM_OPTIONS: MayamSummon[] = [
         return 4000 * 5;
       } else return globalOptions.prefs.valueOfAdventure * 5;
     },
-    type: "ring",
     ring: 4,
   },
 ];
 
-function getBestMayamSummons(options: MayamSummon[]): MayamSummon[] {
-  const filteredOptions = options.filter(
-    ({ choice }) => !get("_mayamSymbolsUsed").includes(choice),
-  );
+const AVAILABLE_RING_OPTIONS: MayamRingSummon[] = MAYAM_RING_OPTIONS.filter(
+  (option) => {
+    // Translate "yam" to "yam1", "yam2", etc.
+    const choice = option.choice.startsWith("yam")
+      ? `yam${option.ring}`
+      : option.choice;
+    // Check if the translated choice appears in yamUses
+    return !yamUses().includes(choice);
+  },
+);
 
-  const sortedOptions = filteredOptions.sort((a, b) => b.value() - a.value());
-  const selectedOptions: MayamSummon[] = [];
-  const selectedRings: Set<number | undefined> = new Set();
+/*
+const MAYAM_RESONANCE_OPTIONS: MayamResonanceSummon[] = [
+  {
+    choice: "yam meat eyepatch yam",
+    value: () => 0.05 * 275, // I don't know how to value a 55% meat accessory
+    components: ["yam1", "meat", "eyepatch", "yam4"]
+  },
+  {
+    choice: "eye yam eyepatch yam",
+    value: () => garboValue($item`Mayam spinach`),
+    components: ["eye", "yam2", "eyepatch", "yam4"]
+  },
+  {
+    choice: "vessel yam cheese explosion",
+    value: () => garboValue($item`stuffed yam stinkbomb`),
+    components: ["vessel", "yam2", "cheese", "explosion"]
+  },
+  {
+    choice: "yam yam yam explosion",
+    value: () => garboValue($item`thanksgiving bomb`),
+    components: ["yam1", "yam2", "yam3", "explosion"]
+  },
+  {
+    choice: "yam meat cheese yam",
+    value: () => garboValue($item`yam and swiss`),
+    components: ["yam1", "meat", "cheese", "yam4"]
+  },
+  {
+    choice: "yam lightning yam clock",
+    value: () => garboValue($item`yam battery`),
+    components: ["yam1", "lightning", "yam3", "clock"]
+  },
+];
+*/
 
-  for (const option of sortedOptions) {
-    if (selectedOptions.length === 3) break;
+function bestRingOptions(): string[] {
+  const bestOptions: string[] = [];
 
-    if (option.type === "resonance") {
-      const choiceItems = option.choice.split(" ");
-      let canSelect = true;
+  for (let ring = 1; ring <= 4; ring++) {
+    // Filter AVAILABLE_RING_OPTIONS for the current ring
+    const ringOptions = AVAILABLE_RING_OPTIONS.filter(
+      (option) => option.ring === ring,
+    );
 
-      for (const item of choiceItems) {
-        const ringOption = selectedOptions.find(
-          (opt) => opt.type === "ring" && opt.choice === item,
-        );
-        if (!ringOption) {
-          canSelect = false;
-          break;
-        }
-      }
+    // If there are no options for the current ring, continue to the next ring
+    if (ringOptions.length === 0) {
+      continue;
+    }
 
-      if (canSelect) {
-        const resonanceValue =
-          option.value() +
-          choiceItems.reduce((acc, item) => {
-            const ringOption = selectedOptions.find(
-              (opt) => opt.type === "ring" && opt.choice === item,
-            );
-            return acc + (ringOption ? ringOption.value() : 0);
-          }, 0);
+    // Map ringOptions to an array of objects with 'choice' and 'value' properties
+    const ringOptionsWithoutRing = ringOptions.map(({ choice, value }) => ({
+      choice,
+      value,
+    }));
 
-        selectedOptions.push({
-          choice: option.choice,
-          value: () => resonanceValue,
-          type: "resonance",
-        });
-        choiceItems.forEach((item) => {
-          const ringOptionIndex = selectedOptions.findIndex(
-            (opt) => opt.type === "ring" && opt.choice === item,
-          );
-          selectedOptions.splice(ringOptionIndex, 1);
-        });
-      }
-    } else if (option.type === "ring" && !selectedRings.has(option.ring)) {
-      selectedOptions.push(option);
-      selectedRings.add(option.ring);
+    // Find the best option for the current ring
+    const bestOption = maxBy(ringOptionsWithoutRing, (option) =>
+      option.value(),
+    );
+
+    // Add the choice of the best option to the result array
+    if (bestOption) {
+      bestOptions.push(bestOption.choice);
     }
   }
 
-  return selectedOptions;
+  return bestOptions;
 }
 
 function summonTask(): GarboTask {
@@ -228,10 +211,11 @@ function summonTask(): GarboTask {
         get("_mayamSymbolsUsed").includes(sym),
       ),
     do: () => {
-      const mayamSummons = getBestMayamSummons(MAYAM_OPTIONS);
-      mayamSummons.forEach((summon) => {
-        const command = `mayam ${summon.choice}`;
-        cliExecute(command);
+      const ringOptions = bestRingOptions();
+      ringOptions.forEach((choice, index) => {
+        const command = `mayam ${choice}`;
+        // Adjust ring index (1-based) and add 1 turn delay between summons
+        setTimeout(() => cliExecute(command), index * 1000);
       });
     },
     spendsTurn: false,
