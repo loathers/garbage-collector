d7d2b3aafac8c0bbbe5d86868db7393189710584
Shiverwarp <Lorenz69@gmail.com>
2024-06-04 18:35:42 -0600

    Don't bother checking for being in a clan

--- packages/garbo/src/resources/clanVIP.ts
+++ packages/garbo/src/resources/clanVIP.ts
@@ -1,8 +1,5 @@
-import { $item, Clan, get, have } from "libram";
+import { $item, get, have } from "libram";
 
 export function hotTubAvailable(): boolean {
-  return (
-    // FIXME getClanID returns a clan even if you aren't currently in one, as long as you were ever in a clan.
-    Clan.get() && have($item`Clan VIP Lounge key`) && get("_hotTubSoaks") < 5
-  );
+  return have($item`Clan VIP Lounge key`) && get("_hotTubSoaks") < 5;
 }
