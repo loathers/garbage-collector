3c906b11d7661d50fcd16111c4c42dc294fd5749
Ignose <ihpares@gmail.com>
2024-07-03 14:53:22 -0400

    Boy do I hate Garbo Choice

--- packages/garbo/garbo_choice.ts
+++ packages/garbo/garbo_choice.ts
@@ -1,51 +1,57 @@
 import { availableChoiceOptions, runChoice } from "kolmafia";
+import { maxBy } from "libram";
 
-type DartPriority = {
-  [key: string]: number;
+type DartPerk = {
+  Perk: string;
+  Rank: number;
 };
 
+const dartPriority: DartPerk[] = [
+  { Perk: "Throw a second dart quickly", Rank: 8 },
+  { Perk: "Deal 25-50% more damage", Rank: 16 },
+  { Perk: "You are less impressed by bullseyes", Rank: 2 },
+  { Perk: "25% Better bullseye targeting", Rank: 5 },
+  { Perk: "Extra stats from stats targets", Rank: 6 },
+  { Perk: "Butt awareness", Rank: 9 },
+  { Perk: "Add Hot Damage", Rank: 11 },
+  { Perk: "Add Cold Damage", Rank: 12 },
+  { Perk: "Add Sleaze Damage", Rank: 13 },
+  { Perk: "Add Spooky Damage", Rank: 14 },
+  { Perk: "Add Stench Damage", Rank: 15 },
+  { Perk: "Expand your dart capacity by 1", Rank: 7 },
+  { Perk: "Bullseyes do not impress you much", Rank: 1 },
+  { Perk: "25% More Accurate bullseye targeting", Rank: 4 },
+  { Perk: "Deal 25-50% extra damage", Rank: 17 },
+  { Perk: "Increase Dart Deleveling from deleveling targets", Rank: 10 },
+  { Perk: "Deal 25-50% greater damage", Rank: 18 },
+  { Perk: "25% better chance to hit bullseyes", Rank: 3 },
+];
+
+function highestPriorityOption(choices: { [key: number]: string }) {
+  const perkRanks = dartPriority.reduce(
+    (acc, perk) => {
+      acc[perk.Perk] = perk.Rank;
+      return acc;
+    },
+    {} as { [key: string]: number },
+  );
+
+  // Create an array of [choice number, rank] pairs
+  const validChoices = Object.entries(choices)
+    .filter(([choiceText]) => choiceText in perkRanks)
+    .map(([choiceNumber, choiceText]) => ({
+      choiceNumber: parseInt(choiceNumber),
+      rank: perkRanks[choiceText],
+    }));
+
+  // Use maxBy to find the choice with the lowest rank (highest priority)
+  const bestChoice = maxBy(validChoices, (choice) => -choice.rank);
+
+  return bestChoice.choiceNumber;
+}
+
 export function main() {
-  const dartPriority: DartPriority = {
-    "Throw a second dart quickly": 60,
-    "Deal 25-50% more damage": 800,
-    "You are less impressed by bullseyes": 10,
-    "25% Better bullseye targeting": 20,
-    "Extra stats from stats targets": 40,
-    "Butt awareness": 30,
-    "Add Hot Damage": 1000,
-    "Add Cold Damage": 1000,
-    "Add Sleaze Damage": 1000,
-    "Add Spooky Damage": 1000,
-    "Add Stench Damage": 1000,
-    "Expand your dart capacity by 1": 50,
-    "Bullseyes do not impress you much": 9,
-    "25% More Accurate bullseye targeting": 19,
-    "Deal 25-50% extra damage": 10000,
-    "Increase Dart Deleveling from deleveling targets": 100,
-    "Deal 25-50% greater damage": 10000,
-    "25% better chance to hit bullseyes": 18,
-  };
+  const options = availableChoiceOptions();
 
-  function getLowestPriorityOption(): number {
-    const options = availableChoiceOptions();
-    let lowestPriority = Infinity;
-    let bestOptionKey: number = 1;
-
-    for (const key in options) {
-      if (Object.prototype.hasOwnProperty.call(options, key)) {
-        const optionKey = Number(key);
-        const option = options[optionKey];
-        const priority = dartPriority[option];
-
-        if (priority !== undefined && priority < lowestPriority) {
-          lowestPriority = priority;
-          bestOptionKey = optionKey;
-        }
-      }
-    }
-
-    return bestOptionKey;
-  }
-
-  runChoice(getLowestPriorityOption());
+  runChoice(highestPriorityOption(options));
 }
--- packages/garbo/src/resources/darts.ts
+++ packages/garbo/src/resources/darts.ts
@@ -6,15 +6,12 @@ export const guaranteedBullseye = () =>
   get("everfullDartPerks").includes("25% better chance to hit bullseyes");
 
 export const safeToAttemptBullseye = () =>
-  guaranteedBullseye()
-    ? have($item`Everfull Dart Holster`)
-    : have($item`Everfull Dart Holster`) && have($item`spring shoes`);
+  have($item`Everfull Dart Holster`) &&
+  (guaranteedBullseye() || have($item`spring shoes`));
 
 export const canBullseye = () =>
-  guaranteedBullseye()
-    ? have($effect`Everything Looks Red`)
-    : have($effect`Everything Looks Red`) ||
-      have($effect`Everything Looks Green`);
+  have($effect`Everything Looks Red`) &&
+  (guaranteedBullseye() || !have($effect`Everything Looks Green`));
 
 export const DARTS_KILL_BEFORE_RUN = 5;
 
