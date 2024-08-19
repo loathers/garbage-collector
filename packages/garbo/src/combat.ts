7fcefdb742715cd43d67861294fea7ac806964f9
neil <78829653+horrible-little-slime@users.noreply.github.com>
2024-08-19 16:51:05 -0400

    don't LTS with ranged weapons (#2023)
    * don't LTS with ranged weapons
    
    * what about knives?

--- packages/garbo/src/combat.ts
+++ packages/garbo/src/combat.ts
@@ -2,6 +2,7 @@ import "core-js/features/array/flat";
 import {
   adv1,
   choiceFollowsFight,
+  currentHitStat,
   equippedAmount,
   equippedItem,
   getAutoAttack,
@@ -593,7 +594,9 @@ export class Macro extends StrictMacro {
         ),
       )
       .externalIf(
-        myBuffedstat($stat`Muscle`) > myBuffedstat($stat`Mysticality`),
+        myBuffedstat($stat`Muscle`) > myBuffedstat($stat`Mysticality`) &&
+          (currentHitStat() === $stat`Muscle` ||
+            itemType(equippedItem($slot`weapon`)) === "knife"),
         Macro.trySkillRepeat(
           $skill`Northern Explosion`,
           $skill`Lunging Thrust-Smack`,
