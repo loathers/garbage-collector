c8d95d5ad4067074604bfb36423263de07a0d491
Rinn <Kirchoff.Joseph.P@gmail.com>
2024-08-22 04:14:08 +0000

    add packages for windows (#2029)

--- scripts/garbage-collector/garbo.js
+++ scripts/garbage-collector/garbo.js
@@ -29045,7 +29045,7 @@ function checkGithubVersion() {
     var releaseSHA = (_gitBranches$find = gitBranches.find(function(branchInfo) {
       return branchInfo.name === "release";
     })) === null || _gitBranches$find === void 0 || (_gitBranches$find = _gitBranches$find.commit) === null || _gitBranches$find === void 0 ? void 0 : _gitBranches$find.sha;
-    (0, import_kolmafia83.print)("Local Version: ".concat(localSHA, " (built from ").concat("main", "@").concat("8ac26d6f5b8ca6a0fe20737fcae8408f2a68ce97", ")"));
+    (0, import_kolmafia83.print)("Local Version: ".concat(localSHA, " (built from ").concat("main", "@").concat("9bb8a97271ea43c32e8cb84daaf07759a2f127a1", ")"));
     if (releaseSHA === localSHA) {
       (0, import_kolmafia83.print)("Garbo is up to date!", HIGHLIGHT);
     } else if (releaseSHA === void 0) {
