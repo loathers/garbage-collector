bc5d975e5e2cc93dc718d3d10d2c96613963b056
Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>
2024-09-27 23:59:56 -0700

    Update packages/garbo/src/familiar/lib.ts

--- packages/garbo/src/familiar/lib.ts
+++ packages/garbo/src/familiar/lib.ts
@@ -139,14 +139,14 @@ const ESTIMATED_ENVELOPE_VALUE = 50_000;
 
 export function snapperValue(): number {
   const item = Snapper.phylumItem.get(globalOptions.target.phylum);
+  if (!item) return 0;
+  
   const denominator =
     11 -
     (Snapper.getTrackedPhylum() === globalOptions.target.phylum
       ? Snapper.getProgress()
       : 0);
   if (denominator > copyTargetCount()) return 0;
-
-  if (!item) return 0;
 
   if (item === $item`envelope full of Meat`) {
     return ESTIMATED_ENVELOPE_VALUE / denominator;
