928fbaf433b88c906db024a60898cd0d489e2143
Ignose <128612824+Ignose@users.noreply.github.com>
2025-02-14 00:25:35 -0500

    Update index.ts

--- packages/garbo/src/familiar/index.ts
+++ packages/garbo/src/familiar/index.ts
@@ -8,4 +8,4 @@ export { barfFamiliar } from "./barfFamiliar";
 export { freeFightFamiliar } from "./freeFightFamiliar";
 export { bestFairy } from "./itemFamiliar";
 export type { FamiliarMenuOptions } from "./freeFightFamiliar";
-export { getToyCupidBowFamiliars } from "./toyCupidBowFamiliars";
+export { getToyCupidBowFamiliars } from "./toyCupidBowFamiliar";
