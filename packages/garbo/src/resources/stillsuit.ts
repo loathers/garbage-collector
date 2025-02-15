234e5b89b3f273e8af59c37f8e9efad08bf437e3
horrible little slime <69secret69email69@gmail.com>
2025-02-14 22:51:23 -0500

    don't bring negatives to noninteger powers

--- packages/garbo/src/resources/stillsuit.ts
+++ packages/garbo/src/resources/stillsuit.ts
@@ -2,7 +2,8 @@ import { StillSuit } from "libram";
 
 const currentAdventures = () => StillSuit.distillateAdventures();
 const nextDistillateSweat = () => (currentAdventures() + 0.5) ** (5 / 2);
-const previousDistillateSweat = () => (currentAdventures() - 0.5) ** (5 / 2);
+const previousDistillateSweat = () =>
+  Math.max(currentAdventures() - 0.5, 0) ** (5 / 2);
 
 export const adventuresPerSweat = () =>
   1 / (nextDistillateSweat() - previousDistillateSweat());
