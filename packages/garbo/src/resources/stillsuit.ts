e01b46b643f40e483d53e8bad32b070b791dd6a0
horrible little slime <69secret69email69@gmail.com>
2025-02-25 22:21:39 -0500

    I think this rounds correctly?

--- packages/garbo/src/resources/stillsuit.ts
+++ packages/garbo/src/resources/stillsuit.ts
@@ -1,9 +1,10 @@
 import { StillSuit } from "libram";
 
 const currentAdventures = () => StillSuit.distillateAdventures();
-const nextDistillateSweat = () => (currentAdventures() + 0.5) ** (5 / 2);
+const nextDistillateSweat = () =>
+  Math.ceil((currentAdventures() + 0.5) ** (5 / 2));
 const previousDistillateSweat = () =>
-  Math.max(currentAdventures() - 0.5, 0) ** (5 / 2);
+  Math.ceil(Math.max(currentAdventures() - 0.5, 0) ** (5 / 2));
 
 export const adventuresPerSweat = () =>
   1 / (nextDistillateSweat() - previousDistillateSweat());
