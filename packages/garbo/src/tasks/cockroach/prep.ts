0bd77ae2585432bcda40d547d6a83faf007e75b2
Shiverwarp <Lorenz69@gmail.com>
2025-03-21 16:05:20 -0600

    comment is no longer relevant

--- packages/garbo/src/tasks/cockroach/prep.ts
+++ packages/garbo/src/tasks/cockroach/prep.ts
@@ -64,10 +64,6 @@ export const CockroachSetup: Quest<GarboTask> = {
       },
       spendsTurn: false,
     },
-    // Tasks to progress pirate realm up to selecting Trash Island go here
-    // We'll have to be careful about things like max stats becoming too high (bofa is annoying for this!)
-    // To be optimal we would progress up until we're about to fight the giant giant crab, and then after buffing and fighting it, we then select trash island.
-    // We might need some restructuring to do this nicely?
     {
       name: "Get PirateRealm Eyepatch",
       completed: () => have($item`PirateRealm eyepatch`),
