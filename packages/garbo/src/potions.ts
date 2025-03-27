d02e8c84a3a70068e8c34bcd79d08e4c112b8220
Shiverwarp <Lorenz69@gmail.com>
2025-03-26 23:18:04 -0600

    Fix duration bug in gross()

--- packages/garbo/src/potions.ts
+++ packages/garbo/src/potions.ts
@@ -284,7 +284,7 @@ export class Potion {
 
   gross(targets: number, maxTurns?: number): number {
     const bonusMeat = this.bonusMeat();
-    const duration = Math.max(this.effectDuration(), maxTurns ?? 0);
+    const duration = Math.min(this.effectDuration(), maxTurns ?? Infinity);
     // Number of meat targets this will actually be in effect for.
     const targetsApplied = Math.max(
       Math.min(duration, targets - haveEffect(this.effect())),
