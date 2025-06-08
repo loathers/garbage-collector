3db487e8178af5c9222687fc5e6fac136bc6e399
Shiverwarp <Lorenz69@gmail.com>
2025-06-08 11:55:11 -0600

    Neil's suggestion

--- packages/garbo/src/tasks/yachtzee/index.ts
+++ packages/garbo/src/tasks/yachtzee/index.ts
@@ -22,103 +22,106 @@ import { getBestWaterBreathingEquipment } from "./lib";
 import { GarboStrategy, Macro } from "../../combat";
 import { GarboTask } from "../engine";
 import { willDrunkAdventure } from "../../lib";
-import { Outfit } from "grimoire-kolmafia";
+import { Outfit, Quest } from "grimoire-kolmafia";
 import { maximumYachtzees, shouldClara, willYachtzee } from "../../resources";
 
 type AlternateTask = GarboTask & { turns: Delayed<number> };
 
-export function yachtzeeTasks(): AlternateTask[] {
-  if (!willYachtzee() && !get("noncombatForcerActive")) return [];
-  return [
-    {
-      name: "Yachtzee",
-      completed: () => !get("noncombatForcerActive"),
-      ready: () => have($effect`Fishy`),
-      choices: { 918: 2 },
-      do: $location`The Sunken Party Yacht`,
-      outfit: () => {
-        const outfit = new Outfit();
-        const overdrunk = myInebriety() > inebrietyLimit();
-        const yachtzeeFamiliar = bestYachtzeeFamiliar();
-        outfit.modifier.push("20 Meat Drop", "-tie");
-        if (
-          !(
-            yachtzeeFamiliar.underwater ||
-            have($effect`Driving Waterproofly`) ||
-            have($effect`Wet Willied`)
-          )
-        ) {
-          outfit.modifier.push("underwater familiar");
-        }
-        outfit.equip(getBestWaterBreathingEquipment(maximumYachtzees()).item);
-        outfit.equip(bestFamUnderwaterGear(yachtzeeFamiliar));
-        if (overdrunk) outfit.equip($item`Drunkula's wineglass`);
-        outfit.avoid.push(
-          ...$items`anemoney clip, cursed magnifying glass, Kramco Sausage-o-Matic™, cheap sunglasses`,
-        );
-        if (outfit.haveEquipped($item`The Crown of Ed the Undying`)) {
-          outfit.setModes({ edpiece: "fish" });
-        }
-        outfit.familiar = yachtzeeFamiliar;
-        return outfit;
-      },
-      combat: new GarboStrategy(() =>
-        Macro.abortWithMsg(
-          "Unexpected combat while attempting yachtzee adventure",
+export const yachtzeeQuest: Quest<AlternateTask>[] = [
+  {
+    name: "Yachtzee",
+    completed: () => !willYachtzee() && !get("noncombatForcerActive"),
+    tasks: [
+      {
+        name: "Yachtzee",
+        completed: () => !get("noncombatForcerActive"),
+        ready: () => have($effect`Fishy`),
+        choices: { 918: 2 },
+        do: $location`The Sunken Party Yacht`,
+        outfit: () => {
+          const outfit = new Outfit();
+          const overdrunk = myInebriety() > inebrietyLimit();
+          const yachtzeeFamiliar = bestYachtzeeFamiliar();
+          outfit.modifier.push("20 Meat Drop", "-tie");
+          if (
+            !(
+              yachtzeeFamiliar.underwater ||
+              have($effect`Driving Waterproofly`) ||
+              have($effect`Wet Willied`)
+            )
+          ) {
+            outfit.modifier.push("underwater familiar");
+          }
+          outfit.equip(getBestWaterBreathingEquipment(maximumYachtzees()).item);
+          outfit.equip(bestFamUnderwaterGear(yachtzeeFamiliar));
+          if (overdrunk) outfit.equip($item`Drunkula's wineglass`);
+          outfit.avoid.push(
+            ...$items`anemoney clip, cursed magnifying glass, Kramco Sausage-o-Matic™, cheap sunglasses`,
+          );
+          if (outfit.haveEquipped($item`The Crown of Ed the Undying`)) {
+            outfit.setModes({ edpiece: "fish" });
+          }
+          outfit.familiar = yachtzeeFamiliar;
+          return outfit;
+        },
+        combat: new GarboStrategy(() =>
+          Macro.abortWithMsg(
+            "Unexpected combat while attempting yachtzee adventure",
+          ),
         ),
-      ),
-      turns: maximumYachtzees,
-      spendsTurn: true,
-    },
-    {
-      name: "Use Fishy Pipe for Yachtzee",
-      completed: () => have($effect`Fishy`),
-      ready: () => have($item`fishy pipe`) && !get("_fishyPipeUsed"),
-      do: () => use($item`fishy pipe`),
-      turns: 0,
-      sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
-      spendsTurn: false,
-    },
-    {
-      name: "Use Skatepark for Yachtzee",
-      completed: () => have($effect`Fishy`),
-      ready: () => get("skateParkStatus") === "ice" && !get("_skateBuff1"),
-      do: () => cliExecute("skate lutz"),
-      turns: 0,
-      sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
-      spendsTurn: false,
-    },
-    {
-      name: "Apriling Band Tuba Yachtzee NC Force",
-      completed: () => get("noncombatForcerActive"),
-      ready: () =>
-        have($item`Apriling band tuba`) &&
-        $item`Apriling band tuba`.dailyusesleft > 0 &&
-        have($effect`Fishy`),
-      do: () => AprilingBandHelmet.play($item`Apriling band tuba`),
-      turns: 0,
-      sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
-      spendsTurn: false,
-    },
-    {
-      name: "Clara Yachtzee NC Force",
-      completed: () => get("noncombatForcerActive"),
-      ready: () => shouldClara("yachtzee") && have($effect`Fishy`),
-      do: () => {
-        use($item`Clara's bell`);
+        turns: maximumYachtzees,
+        spendsTurn: true,
       },
-      turns: 0,
-      sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
-      spendsTurn: false,
-    },
-    {
-      name: "Cincho Yachtzee NC Force",
-      completed: () => get("noncombatForcerActive"),
-      ready: () => CinchoDeMayo.currentCinch() >= 60 && have($effect`Fishy`),
-      do: () => useSkill($skill`Cincho: Fiesta Exit`),
-      turns: 0,
-      sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
-      spendsTurn: false,
-    },
-  ];
-}
+      {
+        name: "Use Fishy Pipe for Yachtzee",
+        completed: () => have($effect`Fishy`),
+        ready: () => have($item`fishy pipe`) && !get("_fishyPipeUsed"),
+        do: () => use($item`fishy pipe`),
+        turns: 0,
+        sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
+        spendsTurn: false,
+      },
+      {
+        name: "Use Skatepark for Yachtzee",
+        completed: () => have($effect`Fishy`),
+        ready: () => get("skateParkStatus") === "ice" && !get("_skateBuff1"),
+        do: () => cliExecute("skate lutz"),
+        turns: 0,
+        sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
+        spendsTurn: false,
+      },
+      {
+        name: "Apriling Band Tuba Yachtzee NC Force",
+        completed: () => get("noncombatForcerActive"),
+        ready: () =>
+          have($item`Apriling band tuba`) &&
+          $item`Apriling band tuba`.dailyusesleft > 0 &&
+          have($effect`Fishy`),
+        do: () => AprilingBandHelmet.play($item`Apriling band tuba`),
+        turns: 0,
+        sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
+        spendsTurn: false,
+      },
+      {
+        name: "Clara Yachtzee NC Force",
+        completed: () => get("noncombatForcerActive"),
+        ready: () => shouldClara("yachtzee") && have($effect`Fishy`),
+        do: () => {
+          use($item`Clara's bell`);
+        },
+        turns: 0,
+        sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
+        spendsTurn: false,
+      },
+      {
+        name: "Cincho Yachtzee NC Force",
+        completed: () => get("noncombatForcerActive"),
+        ready: () => CinchoDeMayo.currentCinch() >= 60 && have($effect`Fishy`),
+        do: () => useSkill($skill`Cincho: Fiesta Exit`),
+        turns: 0,
+        sobriety: () => (willDrunkAdventure() ? "drunk" : "sober"),
+        spendsTurn: false,
+      },
+    ],
+  },
+];
