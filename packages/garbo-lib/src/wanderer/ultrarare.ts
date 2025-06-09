957a2a602c6b2726e542ed396584c0ce6400795a
Shiverwarp <Lorenz69@gmail.com>
2025-06-09 15:23:10 -0600

    update mafia revision, add existence check

--- packages/garbo-lib/src/wanderer/ultrarare.ts
+++ packages/garbo-lib/src/wanderer/ultrarare.ts
@@ -23,7 +23,7 @@ export function ultraRareFactory(
             itemDropsArray(
               getMonsters(z).find((m) => m.attributes.includes("ULTRARARE")) ??
                 $monster.none,
-            )[0].drop ?? $item.none,
+            )[0]?.drop ?? $item.none,
           ) / 500_000_000, // Ultra rares are rare, let's say 1 in 500 million to be conservative
         ),
     );
