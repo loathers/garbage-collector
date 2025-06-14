d747cc2d64be3b7947ecfdd71ed9a9cbd63c4715
Shiverwarp <Lorenz69@gmail.com>
2025-06-13 19:56:29 -0600

    Actually write to constructedLocations

--- packages/garbo-lib/src/wanderer/index.ts
+++ packages/garbo-lib/src/wanderer/index.ts
@@ -205,6 +205,7 @@ function bestWander(
     } else {
       wandererLocation.value += monsterAverageValue;
     }
+    constructedLocations.set(location, wandererLocation);
   }
 
   if (constructedLocations.size === 0) {
