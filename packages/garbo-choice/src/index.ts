a6e87ca9a086e232fb8c5dd8f82a88061c0f575d
horrible little slime <69secret69email69@gmail.com>
2025-08-20 18:50:43 -0400

    better generic handling for choices

--- packages/garbo-choice/src/index.ts
+++ packages/garbo-choice/src/index.ts
@@ -1,15 +1,20 @@
 import { ChoiceAdventureScript, runChoice } from "kolmafia";
 import { getBestMobiusOption } from "./resources";
+import { get, NumericOrStringProperty } from "libram";
 
 export const main: ChoiceAdventureScript = (choiceNumber) => {
   switch (choiceNumber) {
-    case 1224:
-      return void runChoice(3); // LOV Epaulettes
-    case 1226:
-      return void runChoice(2); // Open Heart Surgery
-    case 1228:
-      return void runChoice(3); // LOV ExtraTerrestrial Chocolate
     case 1562:
       return void runChoice(getBestMobiusOption());
+    default: {
+      const option = get(
+        `choiceAdventure${choiceNumber}` as NumericOrStringProperty,
+      );
+      if (option) {
+        if (typeof option === "number") return void runChoice(option);
+        const [numeric, params] = option.split("&", 2);
+        return void runChoice(Number(numeric), params);
+      }
+    }
   }
 };
