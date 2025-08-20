b256196f3f07fc8e5109907cc151ee3b1315981c
horrible little slime <69secret69email69@gmail.com>
2025-08-20 19:33:48 -0400

    use the ARB to get radios

--- packages/garbo/src/tasks/dailyItems.ts
+++ packages/garbo/src/tasks/dailyItems.ts
@@ -1,6 +1,7 @@
 import { AcquireItem, Quest } from "grimoire-kolmafia";
 import {
   abort,
+  alliedRadio,
   buy,
   canAdventure,
   canEquip,
@@ -734,6 +735,14 @@ const DailyItemTasks: GarboTask[] = [
     spendsTurn: false,
   },
   leprecondoTask(),
+  {
+    name: "Allied Radio Backpack",
+    ready: () => have($item`Allied Radio Backpack`),
+    completed: () => get("_alliedRadioDropsUsed") >= 3,
+    do: () => alliedRadio("radio"),
+    limit: { skip: 3 },
+    spendsTurn: false,
+  },
 ];
 
 export const DailyItemsQuest: Quest<GarboTask> = {
