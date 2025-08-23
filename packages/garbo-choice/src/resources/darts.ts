261945a94cacf6dfff895c3891c1b036a8bbf844
horrible little slime <69secret69email69@gmail.com>
2025-08-23 11:44:29 -0400

    remove redundant functions from choice resources file

--- packages/garbo-choice/src/resources/darts.ts
+++ packages/garbo-choice/src/resources/darts.ts
@@ -1,24 +1,5 @@
 import { availableChoiceOptions } from "kolmafia";
-import { $effect, $item, get, have, maxBy } from "libram";
-
-export const guaranteedBullseye = () =>
-  get("everfullDartPerks").includes("25% Better bullseye targeting") &&
-  get("everfullDartPerks").includes("25% More Accurate bullseye targeting") &&
-  get("everfullDartPerks").includes("25% better chance to hit bullseyes");
-
-export const safeToAttemptBullseye = () =>
-  have($item`Everfull Dart Holster`) &&
-  (guaranteedBullseye() || have($item`spring shoes`)) &&
-  !dartLevelTooHigh();
-
-export const canBullseye = () =>
-  !have($effect`Everything Looks Red`) &&
-  (guaranteedBullseye() || !have($effect`Everything Looks Green`));
-
-export const DARTS_KILL_BEFORE_RUN = 5;
-
-const dartLevelTooHigh = () =>
-  get("everfullDartPerks").split(",").length >= DARTS_KILL_BEFORE_RUN;
+import { maxBy } from "libram";
 
 export const DART_PERKS: string[] = [
   "Bullseyes do not impress you much",
