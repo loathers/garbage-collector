b00b167ea99582bbb945cf30214815b628f2e382
horrible little slime <69secret69email69@gmail.com>
2025-08-22 21:24:34 -0400

    peridot error catching

--- packages/garbo-choice/src/index.ts
+++ packages/garbo-choice/src/index.ts
@@ -1,11 +1,23 @@
-import { ChoiceAdventureScript, runChoice } from "kolmafia";
+import { ChoiceAdventureScript, runChoice, xpath } from "kolmafia";
 import { getBestMobiusOption } from "./resources";
 import { get, NumericOrStringProperty } from "libram";
 
-export const main: ChoiceAdventureScript = (choiceNumber) => {
+export const main: ChoiceAdventureScript = (choiceNumber, pageText) => {
   switch (choiceNumber) {
     case 536: // Map for pills
       return; // Doesn't follow traditional choice adventure structure
+    case 1557: {
+      const option = get("choiceAdventure1557" as NumericOrStringProperty);
+      if (typeof option === "string") {
+        const monsterId = option.slice("1&bandersnatch=".length);
+        if (
+          !xpath(pageText, `//form//input[@bandersnatch='${monsterId}']`).length
+        ) {
+          return void runChoice(2);
+        }
+      }
+      return;
+    }
     case 1562:
       return void runChoice(getBestMobiusOption());
     default: {
