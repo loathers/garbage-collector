378b6cf18145e357c8f338de9753410ddf3ce7bc
Shiverwarp <Lorenz69@gmail.com>
2025-08-29 13:12:24 -0600

    Mafia does special lights out handling

--- packages/garbo-choice/src/index.ts
+++ packages/garbo-choice/src/index.ts
@@ -11,6 +11,20 @@ export const main: ChoiceAdventureScript = (choiceNumber, pageText) => {
     case 536: // Map for pills
     case 914: // Louvre it or Leave it
     case 1499: // Labyrinth of shadows
+    case 890: // Lights Out
+    case 891: //
+    case 892: //
+    case 893: //
+    case 894: //
+    case 895: //
+    case 896: //
+    case 897: //
+    case 898: //
+    case 899: //
+    case 900: //
+    case 901: //
+    case 902: //
+    case 903: // Lights Out
       return; // Doesn't follow traditional choice adventure structure
     case 1525:
       return void runChoice(getBestDartsOption());
