26c562084d5e7d6f1b6d66ef3c13d150e26b310c
horrible little slime <69secret69email69@gmail.com>
2025-09-02 17:48:12 -0400

    cap price of busks at effect price

--- packages/garbo/src/potions.ts
+++ packages/garbo/src/potions.ts
@@ -71,7 +71,7 @@ import {
 import { usingPurse } from "./outfit";
 import { estimatedGarboTurns, highMeatMonsterCount } from "./turns";
 import { globalOptions } from "./config";
-import { castAugustScepterBuffs } from "./resources";
+import { beretEffectValue, castAugustScepterBuffs } from "./resources";
 
 export type PotionTier = "target" | "overlap" | "barf" | "ascending";
 const banned = $items`Uncle Greenspan's Bathroom Finance Guide`;
@@ -720,13 +720,7 @@ function useBusks() {
     return;
   }
   for (let i = get("_beretBuskingUses"); i < 5; i++) {
-    PrismaticBeret.buskFor(
-      {
-        "Familiar Weight": (marginalFamWeightValue() / 100) * baseMeat(),
-        "Meat Drop": baseMeat() / 100,
-      },
-      {},
-    );
+    PrismaticBeret.buskFor(beretEffectValue, {});
   }
 }
 
---
+++ packages/garbo/src/resources/beret.ts
@@ -0,0 +1,37 @@
+import { Effect, effectsModifier, Item, Skill, toSkill } from "kolmafia";
+import {
+  getAcquirePrice,
+  getModifier,
+  have,
+  NumericModifier,
+  sum,
+} from "libram";
+import { baseMeat, marginalFamWeightValue } from "../lib";
+
+export function beretEffectValue(effect: Effect, duration: number) {
+  const skill = toSkill(effect);
+  if (skill !== Skill.none && have(skill)) return 0;
+  const meatValue =
+    duration *
+    sum(
+      [
+        {
+          modifier: "Meat Drop",
+          value: baseMeat() / 100,
+        },
+        {
+          modifier: "Familiar Weight",
+          value: (marginalFamWeightValue() * baseMeat()) / 100,
+        },
+      ],
+      ({ modifier, value }: { modifier: NumericModifier; value: number }) =>
+        value * getModifier(modifier, effect),
+    );
+  const potionPrices = Item.all()
+    .filter((i) => i.tradeable && effectsModifier(i, "Effect").includes(effect))
+    .map(
+      (i) =>
+        (getAcquirePrice(i) * duration) / getModifier("Effect Duration", i),
+    );
+  return Math.min(meatValue, ...potionPrices);
+}
