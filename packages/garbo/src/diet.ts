3dea903d61a216ba5600b1e3f29dbf371a1c8336
Shiverwarp <Lorenz69@gmail.com>
2025-12-19 18:13:53 -0700

    add baked bone meal to diet

--- packages/garbo/src/diet.ts
+++ packages/garbo/src/diet.ts
@@ -627,6 +627,7 @@ function menu(): MenuItem<Note>[] {
     new MenuItem($item`blood-drive sticker`),
     new MenuItem(cheapestItem(standardSpleenItems)),
     new MenuItem(cheapestItem($items`not-a-pipe, glimmering roc feather`)),
+    new MenuItem($item`baked bone meal`), // Remove after crimbo
 
     // MISC
     ...limitedItems,
