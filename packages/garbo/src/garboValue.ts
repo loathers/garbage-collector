7d99f3c38dc3dce885ad9cc341fd1ed7ae62ca8f
Patrick Stalcup <legomaniac0117@gmail.com>
2026-02-10 11:22:33 -0500

    Add price management script

--- packages/garbo/src/garboValue.ts
+++ packages/garbo/src/garboValue.ts
@@ -1,20 +1,14 @@
-import { fileToBuffer, Item, toItem } from "kolmafia";
+import { Item } from "kolmafia";
 import { makeValue, ValueFunctions } from "garbo-lib";
 
 import { $item } from "libram";
+import { readItemValues } from "./price_garbo";
 
 let _valueFunctions: ValueFunctions | undefined;
 function garboValueFunctions(): ValueFunctions {
   if (!_valueFunctions) {
-    const itemValuesStr = fileToBuffer("garbo_item_values.json");
     const itemValues = new Map([[$item`fake hand`, 50_000]]);
-    if (itemValuesStr.length > 0) {
-      const val: { [item: string]: number } = JSON.parse(itemValuesStr);
-      const parsedItems: [Item, number][] = Object.entries(val.items).map(
-        ([itemStr, quantity]) => [toItem(itemStr), quantity],
-      );
-      parsedItems.forEach((e) => itemValues.set(e[0], e[1]));
-    }
+    readItemValues().forEach((value, item) => itemValues.set(item, value));
     _valueFunctions = makeValue({
       itemValues,
     });
---
+++ packages/garbo/src/price_garbo.ts
@@ -0,0 +1,75 @@
+import {
+  bufferToFile,
+  fileToBuffer,
+  Item,
+  print,
+  toFloat,
+  toItem,
+} from "kolmafia";
+
+const FILE_PATH = "garbo_item_values.json";
+
+export function readItemValues(): Map<Item, number> {
+  const itemValuesStr = fileToBuffer(FILE_PATH);
+  const itemValues = new Map<Item, number>();
+  if (itemValuesStr.length > 0) {
+    const val: { [item: string]: number } = JSON.parse(itemValuesStr);
+    const parsedItems: [Item, number][] = Object.entries(val.items).map(
+      ([itemStr, price]) => [toItem(itemStr), price],
+    );
+    parsedItems.forEach((e) => itemValues.set(e[0], e[1]));
+  }
+  return itemValues;
+}
+export function writeItemValues(itemValues: Map<Item, number>) {
+  bufferToFile(JSON.stringify(Object.fromEntries(itemValues)), FILE_PATH);
+}
+
+function list(): void {
+  readItemValues().forEach((price, item) => print(`${item}: ${price}`));
+}
+
+function add(item: Item, price: number) {
+  const map = readItemValues();
+  map.set(item, price);
+  writeItemValues(map);
+}
+
+function remove(item: Item) {
+  const map = readItemValues();
+  map.delete(item);
+  writeItemValues(map);
+}
+
+export function main(argString = ""): void {
+  const parts = argString.split(" ");
+  let valid = false;
+  let price = 0;
+
+  if (
+    parts.length > 1 &&
+    (parts[parts.length - 1].match(/-1/) ||
+      parts[parts.length - 1].match(/\d+/))
+  ) {
+    price = toFloat(parts[parts.length - 1]);
+    valid = true;
+  }
+  const item = toItem(parts.slice(0, -1).join(" "));
+  if (item === Item.none) {
+    valid = false;
+  }
+  if (!valid || argString === "help" || argString === "") {
+    print(
+      "garbo-price: help | list | [item] [price]\n" +
+        "  help: print this help\n" +
+        "  list: print all items and their prices from the file\n" +
+        "  [item] [price]: add an item to the list @ price (use price of -1 to remove from the list)",
+    );
+  } else if (argString === "list") {
+    list();
+  } else if (price === -1) {
+    remove(item);
+  } else {
+    add(item, price);
+  }
+}
