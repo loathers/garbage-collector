7a87d7b9280b65de4387e84711278dd7ab63d247
Patrick Stalcup <legomaniac0117@gmail.com>
2026-02-11 09:52:18 -0500

    Add ability to override item values (#2613)
    * Add ability to override item values
    
    * Add price management script
    
    * Fix small issue, make list work correctly
    
    * Add warning messages. Clean up garbo-price functionality

--- packages/garbo/src/garboValue.ts
+++ packages/garbo/src/garboValue.ts
@@ -2,12 +2,27 @@ import { Item } from "kolmafia";
 import { makeValue, ValueFunctions } from "garbo-lib";
 
 import { $item } from "libram";
+import { printPriceOverrideWarning, readItemValues } from "./price_garbo";
+import { printLog } from "./lib";
 
 let _valueFunctions: ValueFunctions | undefined;
 function garboValueFunctions(): ValueFunctions {
-  return (_valueFunctions ??= makeValue({
-    itemValues: new Map([[$item`fake hand`, 50_000]]),
-  }));
+  if (!_valueFunctions) {
+    const itemValues = new Map([[$item`fake hand`, 50_000]]);
+    const overrideItemValues = readItemValues();
+    if (overrideItemValues.size > 0) {
+      printPriceOverrideWarning();
+      printLog("GARBO PRICE OVERRIDES");
+      overrideItemValues.forEach((value, item) => {
+        printLog(`${item}: ${value}`);
+        itemValues.set(item, value);
+      });
+    }
+    _valueFunctions = makeValue({
+      itemValues,
+    });
+  }
+  return _valueFunctions;
 }
 
 export function garboValue(item: Item): number {
