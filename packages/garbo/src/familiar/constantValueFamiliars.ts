1e673d6aaff07ea254276b802913597162f1db22
Malibu Stacey <50261170+Malibu-Stacey@users.noreply.github.com>
2026-03-02 16:35:31 -0800

    Add XO Skeleton & Mechanical Songbird to ConstantValueFamiliars

--- packages/garbo/src/familiar/constantValueFamiliars.ts
+++ packages/garbo/src/familiar/constantValueFamiliars.ts
@@ -147,6 +147,14 @@ const standardFamiliars: ConstantValueFamiliar[] = [
       2,
     worksOnFreeRun: true,
   },
+  {
+    familiar: $familiar`XO Skeleton`,
+    value: () => garboAverageValue(...$items`X, O`) / 18,
+  },
+  {
+    familiar: $familiar`Mechanical Songbird`,
+    value: () => garboValue($item`intricate clockwork egg`) / 150,
+  },
 ];
 
 function peaceTurkeyDropChance(): number {
