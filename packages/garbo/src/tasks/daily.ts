5b635a5af047d0bbe6f90e2bb3aaecc5b3007888
TommyDingle <121583891+TommyDingle@users.noreply.github.com>
2026-07-16 18:34:03 -0400

    Added a visit to the Speakeasy Pool Table if the player has access (#2683)
    * Added a visit to the Speakeasy Pool Table if the player has access
    
    * Made a mistake with how I flaged the speakeasy as checked, fixed with local flag

--- packages/garbo/src/tasks/daily.ts
+++ packages/garbo/src/tasks/daily.ts
@@ -97,6 +97,7 @@ const retrieveItems = $items`Half a Purse, seal tooth, The Jokester's gun`;
 
 let latteRefreshed = false;
 let snojoConfigured = false;
+let speakeasyPoolTableVisited = false;
 
 // For this valuation, we are using the rough approximated value of different
 //   voting initiatives. They are relatively straghtforward:
@@ -556,6 +557,17 @@ const DailyTasks: GarboTask[] = [
     ready: () => have($item`Glenn's golden dice`),
     completed: () => get("_glennGoldenDiceUsed"),
     do: () => use($item`Glenn's golden dice`),
+    spendsTurn: false,
+  },
+  {
+    name: "Speakeasy pool table",
+    ready: () => get("ownsSpeakeasy"),
+    completed: () => speakeasyPoolTableVisited,
+    do: () => {
+      visitUrl("place.php?whichplace=speakeasy&action=olivers_pooltable");
+      speakeasyPoolTableVisited = true;
+    },
+    outfit: { modifier: "Pool Skill" },
     spendsTurn: false,
   },
   {
