e9746add5b485c049fce00f3a3f461b14980628d
Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>
2026-08-10 09:35:31 +0100

    try; gain

--- packages/garbo/src/fights.ts
+++ packages/garbo/src/fights.ts
@@ -1872,7 +1872,7 @@ export function deliverThesisIfAble(): void {
     ensureEffect($effect`Triple-Sized`);
     outfit("checkpoint");
   }
-  cliExecute(`gain ${requiredMuscle} muscle`);
+  cliExecute(`try; gain ${requiredMuscle} muscle`);
 
   if (molemanReady()) {
     withMacro(
