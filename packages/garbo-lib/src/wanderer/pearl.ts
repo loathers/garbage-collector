0369dea9ada975ed7feef375b6b605321183dfa5
Ignose <ihpares@gmail.com>
2026-09-04 11:05:40 -0400

    Some tweaks

--- packages/garbo-lib/src/wanderer/pearl.ts
+++ packages/garbo-lib/src/wanderer/pearl.ts
@@ -1,17 +1,18 @@
-import { Location } from "kolmafia";
+import { Element, Location, numericModifier } from "kolmafia";
 import {
   DraggableFight,
   WandererFactoryOptions,
   WandererTarget,
   wandererTurnsAvailableToday,
 } from "./lib";
-import { $effect, $item, $location, get, have } from "libram";
+import { $effect, $element, $item, $location, $modifier, get, have } from "libram";
 
 type PearlTarget = {
   location: Location;
   completionPref: string;
   progressPref: string;
   estimatedProgress: number;
+  maximizeElement: Element;
 };
 const PearlTargets: PearlTarget[] = [
   // estimating progress at some lower rates, 3.3 is with just passives.
@@ -21,32 +22,52 @@ const PearlTargets: PearlTarget[] = [
     completionPref: "_unblemishedPearlTheBriniestDeepests",
     progressPref: "_unblemishedPearlTheBriniestDeepestsProgress",
     estimatedProgress: 3.3,
+    maximizeElement: $element`Cold`
   },
   {
     location: $location`Madness Reef`,
     completionPref: "_unblemishedPearlMadnessReef",
     progressPref: "_unblemishedPearlMadnessReefProgress",
     estimatedProgress: 3.3,
+    maximizeElement: $element`Stench`
   },
   {
     location: $location`Anemone Mine`,
     completionPref: "_unblemishedPearlAnemoneMine",
     progressPref: "_unblemishedPearlAnemoneMineProgress",
     estimatedProgress: 3.3,
+    maximizeElement: $element`Spooky`
   },
   {
     location: $location`The Dive Bar`,
     completionPref: "_unblemishedPearlDiveBar",
     progressPref: "_unblemishedPearlDiveBarProgress",
     estimatedProgress: 3.3,
+    maximizeElement: $element`Sleaze`
   },
   {
     location: $location`The Marinara Trench`,
     completionPref: "_unblemishedPearlMarinaraTrench",
     progressPref: "_unblemishedPearlMarinaraTrenchProgress",
     estimatedProgress: 3.3,
+    maximizeElement: $element`Hot`
   },
 ];
+
+// We need to throw this to the maximizer somehow so that we properly value elemental bonuses
+function elementalBonus(element: Element): number {
+  const resistance = numericModifier(
+    $modifier`${element.toString()} Resistance`
+  );
+
+  const progress = (resistance: number) =>
+    Math.min(0.1, Math.max(0.017, 0.017 * Math.floor(resistance / 3)));
+
+  const turns = (resistance: number) =>
+    Math.ceil(1 / progress(resistance));
+
+  return get("valueOfAdventure", 4000) * (turns(resistance) - turns(resistance + 1));
+}
 
 function turnsRemainingToComplete(
   progressPref: string,
